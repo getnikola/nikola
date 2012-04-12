16d537a8b62ece8605ebe8f8d108c57c55a90fbf
Roberto Alsina <ralsina@netmanagers.com.ar>
2012-04-12 22:06:48 +0000

    enabled search form in site theme

--- dodo.py
+++ dodo.py
@@ -142,6 +142,19 @@ SEARCH_FORM = """
     </form>
     <!-- End of custom search -->
 """ % BLOG_URL
+## This search form is better for the "site" theme where it appears on the navigation bar
+#SEARCH_FORM = """
+    #<!-- Custom search -->
+    #<form method="get" id="search" action="http://duckduckgo.com/" class="navbar-form pull-left">
+        #<input type="hidden" name="sites" value="%s"/>
+        #<input type="hidden" name="k8" value="#444444"/>
+        #<input type="hidden" name="k9" value="#D51920"/>
+        #<input type="hidden" name="kt" value="h"/>
+        #<input type="text" name="q" maxlength="255" placeholder="Search&hellip;" class="span2" style="margin-top: 4px;"/>
+        #<input type="submit" value="DuckDuckGo Search" style="visibility: hidden;" />
+    #</form>
+    #<!-- End of custom search -->
+#""" % BLOG_URL
 
 # Google analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
