a0f0be5c23b366533cbbd30d49ff0b08916f4134
Roberto Alsina <ralsina@netmanagers.com.ar>
2012-04-12 20:12:53 +0000

    i18n string files

---
+++ themes/default/messages/es.py
@@ -0,0 +1,14 @@
+# -*- encoding:utf-8 -*-
+
+MESSAGES = {
+    u"LANGUAGE": u"Español",
+    u"Posts for year %s": u"Posts del año %s",
+    u"Archive": u"Archivo",
+    u"Posts about %s:": u"Posts sobre %s",
+    u"Tags": u"Tags",
+    u"Also available in: ": u"También disponible en: ",
+    u"More posts about": u"Más posts sobre",
+    u"Posted:": u"Publicado:",
+    u"Original site": u"Sitio original",
+    u"Read in english": u"Leer en español",
+}
