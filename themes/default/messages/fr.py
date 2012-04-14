93a2c9b559738ae304e6df6cfe3cabdea21226bd
Pablo SEMINARIO <pabluk@gmail.com>
2012-04-14 23:11:13 +0200

    Added french translation for theme messages

---
+++ themes/default/messages/fr.py
@@ -0,0 +1,14 @@
+# -*- encoding:utf-8 -*-
+
+MESSAGES = {
+    u"LANGUAGE": u"Français",
+    u"Posts for year %s": u"Billets de l'année %s",
+    u"Archive": u"Archives",
+    u"Posts about %s:": u"Billets sur %s",
+    u"Tags": u"Étiquettes",
+    u"Also available in: ": u"Disponible aussi en : ",
+    u"More posts about": u"Plus de billets sur",
+    u"Posted:": u"Publié :",
+    u"Original site": u"Site d'origine",
+    u"Read in english": u"Lire en français",
+}
