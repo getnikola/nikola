6d8a5804da1c919611b9ff39292b1f92b99d809f
Roberto Alsina <ralsina@netmanagers.com.ar>
2012-06-18 12:10:38 -0300

    don't include pages in the prev/next post carousel

--- nikola/nikola.py
+++ nikola/nikola.py
@@ -405,10 +405,11 @@ class Nikola(object):
                 self.timeline.append(post)
             self.timeline.sort(cmp=lambda a, b: cmp(a.date, b.date))
             self.timeline.reverse()
-            for i, p in enumerate(self.timeline[1:]):
-                p.next_post = self.timeline[i]
-            for i, p in enumerate(self.timeline[:-1]):
-                p.prev_post = self.timeline[i+1]
+            post_timeline = [ p for p in self.timeline if p.use_in_feeds ]
+            for i, p in enumerate(post_timeline[1:]):
+                p.next_post = post_timeline[i]
+            for i, p in enumerate(post_timeline[:-1]):
+                p.prev_post = post_timeline[i+1]
             self._scanned = True
             print "done!"
 
