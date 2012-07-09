c79391d1a718c9315f95b084660c24f017c7b74e
Ivan Teoh <contact@ivanteoh.com>
2012-07-09 22:48:52 +1000

    Update time format.

--- nikola/nikola.py
+++ nikola/nikola.py
@@ -84,8 +84,8 @@ class Post(object):
             meta_data.append("")
         default_title, default_pagename, self.date, self.tags, self.link = \
             [x.strip() for x in meta_data][:5]
-        self.date = datetime.datetime.strptime(self.date, '%Y/%m/%d %I:%M:%S %p')
-        #self.date = datetime.datetime.strptime(self.date, '%Y/%m/%d %H:%M:%S')
+        #self.date = datetime.datetime.strptime(self.date, '%Y/%m/%d %I:%M:%S %p')
+        self.date = datetime.datetime.strptime(self.date, '%Y/%m/%d %H:%M:%S')
         self.tags = [x.strip() for x in self.tags.split(',')]
         self.tags = filter(None, self.tags)
         self.compile_html = compile_html
@@ -1120,8 +1120,8 @@ class Nikola(object):
         data = u'\n'.join([
             title,
             slug,
-            datetime.datetime.now().strftime('%Y/%m/%d %I:%M:%S %p')
-            #datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+            #datetime.datetime.now().strftime('%Y/%m/%d %I:%M:%S %p')
+            datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
             ])
         output_path = os.path.dirname(path)
         meta_path = os.path.join(output_path, slug + ".meta")
