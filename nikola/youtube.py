3a1c529ac39517b549309ecd26f17b9f6e514c66
Roberto Alsina <ralsina@netmanagers.com.ar>
2012-07-14 23:20:19 -0300

    style and lint fixes

--- nikola/youtube.py
+++ nikola/youtube.py
@@ -3,10 +3,11 @@ from docutils.parsers.rst import directives
 
 CODE = """\
 <iframe width="%(width)s"
-        height="%(height)s"
-        src="http://www.youtube.com/embed/%(yid)s?rel=0&amp;hd=1&amp;wmode=transparent">
+height="%(height)s"
+src="http://www.youtube.com/embed/%(yid)s?rel=0&amp;hd=1&amp;wmode=transparent">
 </iframe>
 """
+
 
 def youtube(name, args, options, content, lineno,
             contentOffset, blockText, state, stateMachine):
@@ -19,9 +20,9 @@ def youtube(name, args, options, content, lineno,
         'height': 344,
         'extra': ''
         }
-    extra_args = content[1:] # Because content[0] is ID
-    extra_args = [ea.strip().split("=") for ea in extra_args] # key=value
-    extra_args = [ea for ea in extra_args if len(ea) == 2] # drop bad lines
+    extra_args = content[1:]  # Because content[0] is ID
+    extra_args = [ea.strip().split("=") for ea in extra_args]  # key=value
+    extra_args = [ea for ea in extra_args if len(ea) == 2]  # drop bad lines
     extra_args = dict(extra_args)
     if 'width' in extra_args:
         string_vars['width'] = extra_args.pop('width')
