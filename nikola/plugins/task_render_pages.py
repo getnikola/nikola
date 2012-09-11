810d548fd5a04248857da4e547f37717cc6a57de
Roberto Alsina <ralsina@netmanagers.com.ar>
2012-09-11 09:38:01 -0300

    plugified render_pages

---
+++ nikola/plugins/task_render_pages.py
@@ -0,0 +1,36 @@
+import os
+
+from nikola.plugin_categories import Task
+from nikola import utils
+
+
+class CopyAssets(Task):
+    """Copy theme assets into output."""
+
+    name = "copy_assets"
+
+    def gen_tasks(self):
+        """Create tasks to copy the assets of the whole theme chain.
+
+        If a file is present on two themes, use the version
+        from the "youngest" theme.
+        """
+
+        kw = {
+            "themes": self.site.THEMES,
+            "output_folder": self.site.config['OUTPUT_FOLDER'],
+            "filters": self.site.config['FILTERS'],
+        }
+
+        tasks = {}
+        for theme_name in kw['themes']:
+            src = os.path.join(utils.get_theme_path(theme_name), 'assets')
+            dst = os.path.join(kw['output_folder'], 'assets')
+            for task in utils.copy_tree(src, dst):
+                if task['name'] in tasks:
+                    continue
+                tasks[task['name']] = task
+                task['uptodate'] = task.get('uptodate', []) + \
+                    [utils.config_changed(kw)]
+                task['basename'] = self.name
+                yield utils.apply_filters(task, kw['filters'])
