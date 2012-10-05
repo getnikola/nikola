85e94c23c20cb56af2acbd72b9efe4e3f8e99085
Niko Wenselowski <der@nik0.de>
2012-10-05 22:44:27 +0200

    Reworked the tests. The schema validation and link validation are now seperated.

--- tests/test_rss_feeds.py
+++ tests/test_rss_feeds.py
@@ -15,29 +15,6 @@ class RSSFeedTest(unittest.TestCase):
     def setUp(self):
         self.feed_filename = 'testfeed.rss'
 
-    def tearDown(self):
-        if os.path.exists(self.feed_filename):
-            os.remove(self.feed_filename)
-
-    def test_feed_is_valid(self):
-        '''
-        A testcase to check if the generated feed is valid.
-
-        Validation can be tested with W3 FEED Validator that can be found
-        at http://feedvalidator.org
-        '''
-        # This validation regex is taken from django.core.validators
-        url_validation_regex = re.compile(r'^(?:http|ftp)s?://'  # http:// or https://
-                                          r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-                                          r'localhost|'  # localhost...
-                                          r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|'  # ...or ipv4
-                                          r'\[?[A-F0-9]*:[A-F0-9:]+\]?)'  # ...or ipv6
-                                          r'(?::\d+)?'  # optional port
-                                          r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-
-        def is_valid_URL(url):
-            return url_validation_regex.match(url) is not None
-
         def get_metadata(*args, **kwargs):
             return ('post title', 'awesome_article',
                     '2012-10-01 22:41', 'tags', 'link', 'description')
@@ -68,35 +45,56 @@ class RSSFeedTest(unittest.TestCase):
                     self.assertTrue(os.path.exists(self.feed_filename),
                                     'No feed was created!')
 
-                    with open('rss-2_0.xsd', 'r') as rss20_schema_file:
-                        schema_root = etree.XML(''.join(rss20_schema_file.readlines()))
-                        schema = etree.XMLSchema(schema_root)
-                        parser = etree.XMLParser(schema = schema)
+    def tearDown(self):
+        if os.path.exists(self.feed_filename):
+            os.remove(self.feed_filename)
 
-                        with open(self.feed_filename, 'r') as feed_file:
-                            # parsing the created file with the parser will
-                            # throw errors if the scheme is not followed
-                            root = etree.fromstringlist(feed_file.readlines(), parser)
+    def test_feed_items_have_valid_URLs(self):
+        '''The items in the feed need to have valid urls in link and guid.'''
+        # This validation regex is taken from django.core.validators
+        url_validation_regex = re.compile(r'^(?:http|ftp)s?://'  # http:// or https://
+                                          r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+                                          r'localhost|'  # localhost...
+                                          r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|'  # ...or ipv4
+                                          r'\[?[A-F0-9]*:[A-F0-9:]+\]?)'  # ...or ipv6
+                                          r'(?::\d+)?'  # optional port
+                                          r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
-                    et = ElementTree(file=self.feed_filename)
-                    channel = et.find('channel')
-                    item = channel.find('item')
-                    guid = item.find('guid')
-                    link = item.find('link')
+        def is_valid_URL(url):
+            return url_validation_regex.match(url) is not None
 
-                    # 1) "link must be a full and valid URL". We will have to
-                    # use lxml to make links to posts absolute, it seems.
-                    self.assertTrue(is_valid_URL(link.text),
-                                    'The following URL is not valid: %s' %
-                                    link.text)
+        et = ElementTree(file=self.feed_filename)
+        channel = et.find('channel')
+        item = channel.find('item')
+        guid = item.find('guid')
+        link = item.find('link')
 
-                    # 2) "guid must be a full URL, unless isPermaLink attribute
-                    # is false: /weblog/posts/the-minimal-server.html "
-                    # fixable by setting isPermaLink to false, or by using the
-                    # same absolute link as in 1)
-                    self.assertTrue(is_valid_URL(guid.text),
-                                    'The following URL is not valid: %s' %
-                                    guid.text)
+        # As stated by W3 FEED Validator: "link must be a full and valid URL"
+        self.assertTrue(is_valid_URL(link.text),
+                        'The following URL is not valid: %s' % link.text)
+
+        # "guid must be a full URL, unless isPermaLink attribute
+        # is false: /weblog/posts/the-minimal-server.html "
+        self.assertTrue(is_valid_URL(guid.text),
+                        'The following URL is not valid: %s' %
+                        guid.text)
+
+    def test_feed_is_valid(self):
+        '''
+        A testcase to check if the generated feed is valid.
+
+        Validation can be tested with W3 FEED Validator that can be found
+        at http://feedvalidator.org
+        '''
+        with open('rss-2_0.xsd', 'r') as rss20_schema_file:
+            schema_root = etree.XML(''.join(rss20_schema_file.readlines()))
+            schema = etree.XMLSchema(schema_root)
+            parser = etree.XMLParser(schema=schema)
+
+            with open(self.feed_filename, 'r') as feed_file:
+                # parsing the created file with the parser will
+                # throw errors if the scheme is not followed
+                root = etree.fromstringlist(feed_file.readlines(), parser)
 
 
 if __name__ == '__main__':
