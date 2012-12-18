c1eecd0968a678f8a23124d10ed4a62888c24438
Roberto Alsina <ralsina@netmanagers.com.ar>
2012-12-18 10:49:48 -0300

    py3 tweaks

--- nikola/plugins/task_render_tags.py
+++ nikola/plugins/task_render_tags.py
@@ -22,9 +22,9 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+from __future__ import unicode_literals
 import os
 
-from __future__ import unicode_literals
 from nikola.plugin_categories import Task
 from nikola import utils
 
