22fbe3e3e334b7696cf0d8dd75839e86fb9dccad
Roberto Alsina <ralsina@netmanagers.com.ar>
2012-12-18 10:55:06 -0300

    py3 tweaks

--- nikola/data/themes/default/messages/de.py
+++ nikola/data/themes/default/messages/de.py
@@ -1,21 +1,22 @@
 # -*- encoding:utf-8 -*-
+from __future__ import unicode_literals
 
 MESSAGES = {
-    u"LANGUAGE": u"Deutsch",
-    u"Posts for year %s": u"Eintr&auml;ge aus dem Jahr %s",
-    u"Archive": u"Archiv",
-    u"Posts about %s": u"Eintr&auml;ge &uuml;ber %s",
-    u"Tags": u"Tags",
-    u"Also available in": u"Auch verf&uuml;gbar in",
-    u"More posts about": u"Weitere Eintr&auml;ge &uuml;ber",
-    u"Posted": u"Ver&ouml;ffentlicht",
-    u"Original site": u"Original-Seite",
-    u"Read in English": u"Auf Deutsch lesen",
-    u"Older posts": u"&Auml;ltere Eintr&auml;ge",
-    u"Newer posts": u"Neuere Eintr&auml;ge",
-    u"Previous post": u"Vorheriger Eintrag",
-    u"Next post": u"N&auml;chster Eintrag",
-    u"Source": u"Source",
-    u"Read more": u"Weiterlesen",
-    u"old posts page %d": u'Vorherige Eintr&auml;ge %d'
+    "LANGUAGE": "Deutsch",
+    "Posts for year %s": "Eintr&auml;ge aus dem Jahr %s",
+    "Archive": "Archiv",
+    "Posts about %s": "Eintr&auml;ge &uuml;ber %s",
+    "Tags": "Tags",
+    "Also available in": "Auch verf&uuml;gbar in",
+    "More posts about": "Weitere Eintr&auml;ge &uuml;ber",
+    "Posted": "Ver&ouml;ffentlicht",
+    "Original site": "Original-Seite",
+    "Read in English": "Auf Deutsch lesen",
+    "Older posts": "&Auml;ltere Eintr&auml;ge",
+    "Newer posts": "Neuere Eintr&auml;ge",
+    "Previous post": "Vorheriger Eintrag",
+    "Next post": "N&auml;chster Eintrag",
+    "Source": "Source",
+    "Read more": "Weiterlesen",
+    "old posts page %d": u'Vorherige Eintr&auml;ge %d'
 }
