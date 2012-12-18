d25ef4213db8aaa5e35971e1a0c0d9df47d024b0
Roberto Alsina <ralsina@netmanagers.com.ar>
2012-12-18 12:03:14 -0300

    make everything work on py2

--- nikola/plugins/task_render_tags.py
+++ nikola/plugins/task_render_tags.py
@@ -22,7 +22,7 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 import os
 
 from nikola.plugin_categories import Task
