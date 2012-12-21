5585eadf105d602b67941cbef4748c7b4ef3771d
Roberto Alsina <ralsina@netmanagers.com.ar>
2012-12-21 16:44:34 -0300

    oops

--- nikola/plugins/task_sitemap/__init__.py
+++ nikola/plugins/task_sitemap/__init__.py
@@ -22,7 +22,6 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from __future__ import unicode_literals
 import os
 import tempfile
 
