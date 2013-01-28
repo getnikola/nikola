cf640f00124e72f0f5fae504eb57390f08489119
Niko Wenselowski <der@nik0.de>
2013-01-28 21:21:56 +0100

    Making it possible to initialise empty directories.

--- tests/test_command_init.py
+++ tests/test_command_init.py
@@ -11,34 +11,47 @@ class CommandInitCallTest(unittest.TestCase):
     def setUp(self):
         self.copy_sample_site = mock.MagicMock()
         self.create_configuration = mock.MagicMock()
+        self.create_empty_site = mock.MagicMock()
         copy_sample_site_patch = mock.patch('nikola.plugins.command_init.CommandInit.copy_sample_site', self.copy_sample_site)
         create_configuration_patch = mock.patch('nikola.plugins.command_init.CommandInit.create_configuration', self.create_configuration)
+        create_empty_site_patch = mock.patch('nikola.plugins.command_init.CommandInit.create_empty_site', self.create_empty_site)
 
-        self.patches = [copy_sample_site_patch, create_configuration_patch]
+        self.patches = [copy_sample_site_patch, create_configuration_patch, create_empty_site_patch]
         for patch in self.patches:
             patch.start()
 
         self.init_commad = nikola.plugins.command_init.CommandInit()
 
     def tearDown(self):
-        del self.copy_sample_site
-        del self.create_configuration
-
         for patch in self.patches:
             patch.stop()
         del self.patches
+
+        del self.copy_sample_site
+        del self.create_configuration
+        del self.create_empty_site
 
     def test_init_default(self):
         self.init_commad.run('destination')
 
         self.assertTrue(self.create_configuration.called)
         self.assertTrue(self.copy_sample_site.called)
+        self.assertFalse(self.create_empty_site.called)
 
     def test_init_called_without_target(self):
         self.init_commad.run()
 
         self.assertFalse(self.create_configuration.called)
         self.assertFalse(self.copy_sample_site.called)
+        self.assertFalse(self.create_empty_site.called)
+
+    def test_init_empty_dir(self):
+        arguments = ('destination', '--empty')
+        self.init_commad.run(*arguments)
+
+        self.assertTrue(self.create_configuration.called)
+        self.assertFalse(self.copy_sample_site.called)
+        self.assertTrue(self.create_empty_site.called)
 
 
 if __name__ == '__main__':
