dac1b813eae7a9060d089ab6dd74158c018bdc37
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-02-06 20:36:02 -0300

    preparing 5.2

--- setup.py
+++ setup.py
@@ -179,7 +179,7 @@ def find_package_data(
     return out
 
 setup(name='Nikola',
-      version='5.1',
+      version='5.2',
       description='Static blog/website generator',
       author='Roberto Alsina and others',
       author_email='ralsina@netmanagers.com.ar',
