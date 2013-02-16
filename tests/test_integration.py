6f6cf55cf9f9ba86d1fdc35a52ef8db9c0ca94e6
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-02-16 11:18:12 -0300

    don't leave the test site there

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals, print_function
 
 from contextlib import contextmanager
 import os
+import shutil
 import tempfile
 import unittest
 
@@ -18,7 +19,9 @@ def cd(path):
 
 
 class IntegrationTest(unittest.TestCase):
+    """Basic integration testcase."""
     def setUp(self):
+        """Setup a demo site."""
         self.tmpdir = tempfile.mkdtemp()
         self.target_dir = os.path.join(self.tmpdir, "target")
         self.build_command = nikola.plugins.command_build.CommandBuild()
@@ -26,7 +29,12 @@ class IntegrationTest(unittest.TestCase):
         self.init_command.copy_sample_site(self.target_dir)
         self.init_command.create_configuration(self.target_dir)
 
+    def tearDown(self):
+        """Reove the demo site."""
+        shutil.rmtree(self.tmpdir)
+
     def test_default_build(self):
+        """Test that a default build of --demo works."""
         with cd(self.target_dir):
             self.build_command.run()
             self.assertTrue(True)  # Meaning we did not crash ;-)
