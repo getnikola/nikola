6bedaa8cbede69344efc421380e02110e21fe452
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-03-04 12:57:48 -0300

    fix integration tests, added one for Issue 304

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,8 +3,11 @@ from __future__ import unicode_literals, print_function
 
 from contextlib import contextmanager
 import os
+import shutil
 import tempfile
 import unittest
+
+import lxml.html
 
 from context import nikola
 from nikola import main
@@ -40,14 +43,17 @@ class EmptyBuildTest(unittest.TestCase):
     def build(self):
         """Build the site."""
         with cd(self.target_dir):
-            main.main([])
+            main.main(["build"])
 
     def tearDown(self):
-        """Reove the demo site."""
-        #shutil.rmtree(self.tmpdir)
+        """Remove the demo site."""
+        shutil.rmtree(self.tmpdir)
 
     def test_build(self):
-        self.assertTrue(True)
+        """Ensure the build did something."""
+        index_path = os.path.join(
+            self.target_dir, "output", "archive.html")
+        self.assertTrue(os.path.isfile(index_path))
 
 
 class DemoBuildTest(EmptyBuildTest):
@@ -57,3 +63,29 @@ class DemoBuildTest(EmptyBuildTest):
         """Fill the site with demo content."""
         self.init_command.copy_sample_site(self.target_dir)
         self.init_command.create_configuration(self.target_dir)
+
+class RelativeLinkTest(DemoBuildTest):
+    """Check that SITE_URL with a path doesn't break links."""
+
+    def patch_site(self):
+        """Set the SITE_URL to have a path"""
+        conf_path = os.path.join(self.target_dir, "conf.py")
+        with open(conf_path, "rb") as inf:
+            data = inf.read()
+            data.replace('SITE_URL = "http://nikola.ralsina.com.ar"',
+                         'SITE_URL = "http://nikola.ralsina.com.ar/foo/bar/"')
+        with open(conf_path, "wb+") as outf:
+            outf.write(data)
+
+    def test_relative_links(self):
+        """Check that the links in output/index.html are correct"""
+        test_path = os.path.join(self.target_dir, "output", "index.html")
+        with open(test_path, "rb") as inf:
+            data = inf.read()
+            for _, _, url, _ in lxml.html.iterlinks(data):
+                # Just need to be sure this one is ok
+                if "assets/css/all-nocdn.css" in url:
+                    self.assertEqual(url, 'assets/css/all-nocdn.css')
+                    flag = True
+        # But I also need to be sure it is there!
+        self.assertTrue(flag)
