e85fd86d09989f3fd0354fbe682ae587252337cd
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-03-04 22:33:51 -0300

    setup test site per-class, add testcase for Issue #362

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -24,7 +24,8 @@ def cd(path):
 
 class EmptyBuildTest(unittest.TestCase):
     """Basic integration testcase."""
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         """Setup a demo site."""
         self.tmpdir = tempfile.mkdtemp()
         self.target_dir = os.path.join(self.tmpdir, "target")
@@ -33,20 +34,24 @@ class EmptyBuildTest(unittest.TestCase):
         self.patch_site()
         self.build()
 
+    @classmethod
     def fill_site(self):
         """Add any needed initial content."""
         self.init_command.create_empty_site(self.target_dir)
         self.init_command.create_configuration(self.target_dir)
 
+    @classmethod
     def patch_site(self):
         """Make any modifications you need to the site."""
 
+    @classmethod
     def build(self):
         """Build the site."""
         with cd(self.target_dir):
             main.main(["build"])
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(self):
         """Remove the demo site."""
         shutil.rmtree(self.tmpdir)
 
@@ -60,6 +65,7 @@ class EmptyBuildTest(unittest.TestCase):
 class DemoBuildTest(EmptyBuildTest):
     """Test that a default build of --demo works."""
 
+    @classmethod
     def fill_site(self):
         """Fill the site with demo content."""
         self.init_command.copy_sample_site(self.target_dir)
@@ -69,6 +75,7 @@ class DemoBuildTest(EmptyBuildTest):
 class RelativeLinkTest(DemoBuildTest):
     """Check that SITE_URL with a path doesn't break links."""
 
+    @classmethod
     def patch_site(self):
         """Set the SITE_URL to have a path"""
         conf_path = os.path.join(self.target_dir, "conf.py")
@@ -82,6 +89,41 @@ class RelativeLinkTest(DemoBuildTest):
     def test_relative_links(self):
         """Check that the links in output/index.html are correct"""
         test_path = os.path.join(self.target_dir, "output", "index.html")
+        flag = False
+        with open(test_path, "rb") as inf:
+            data = inf.read()
+            for _, _, url, _ in lxml.html.iterlinks(data):
+                # Just need to be sure this one is ok
+                if url.endswith("css"):
+                    self.assertFalse(url.startswith(".."))
+                    flag = True
+        # But I also need to be sure it is there!
+        self.assertTrue(flag)
+
+
+@unittest.expectedFailure
+class RelativeLinkTest2(DemoBuildTest):
+    """Check that dropping stories to the root doesn't break links."""
+
+    @classmethod
+    def patch_site(self):
+        """Set the SITE_URL to have a path"""
+        conf_path = os.path.join(self.target_dir, "conf.py")
+        with codecs.open(conf_path, "rb", "utf-8") as inf:
+            data = inf.read()
+            data = data.replace('("stories/*.txt", "stories", "story.tmpl", False),',
+                                '("stories/*.txt", "", "story.tmpl", False),')
+            data = data.replace('# INDEX_PATH = ""',
+                                'INDEX_PATH = "blog"')
+        with codecs.open(conf_path, "wb+", "utf8") as outf:
+            outf.write(data)
+            outf.flush()
+
+    def test_relative_links(self):
+        """Check that the links in a story are correct"""
+        conf_path = os.path.join(self.target_dir, "conf.py")
+        data = open(conf_path).read()
+        test_path = os.path.join(self.target_dir, "output", "about-nikola.html")
         flag = False
         with open(test_path, "rb") as inf:
             data = inf.read()
