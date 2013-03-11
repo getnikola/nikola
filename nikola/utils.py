be127e9ef72590290ebb5bcb6c8e056ebea67074
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-03-11 17:17:05 -0300

    py3

--- nikola/utils.py
+++ nikola/utils.py
@@ -295,7 +295,7 @@ def slugify(value):
     foo-bar
 
     """
-    if not isinstance(value, unicode):
+    if not isinstance(value, unicode_str):
         raise ValueError("Not a unicode object: {0}".format(value))
     value = unidecode(value)
     # WARNING: this may not be python2/3 equivalent
