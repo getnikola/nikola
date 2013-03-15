80523d72f00bbb2a6113b3ee7a0701335213f1d7
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-03-15 16:10:15 -0300

    transifex translations

---
+++ nikola/data/themes/default/messages/messages_el.py
@@ -0,0 +1,22 @@
+# -*- encoding:utf-8 -*-
+from __future__ import unicode_literals
+
+MESSAGES = {
+"    Also available in": "Διαθέσιμο και στο",
+"    Archive": "Αρχείο",
+"    LANGUAGE": "Ελληνικά",
+"    More posts about": "Περισσότερες αναρτήσεις για",
+"    Newer posts": "Νεότερες αναρτήσεις",
+"    Next post": "Επόμενη ανάρτηση",
+"    Older posts": "Παλαιότερες αναρτήσεις",
+"    Original site": "Ιστοσελίδα αρχικής ανάρτησης",
+"    Posted": "Αναρτήθηκε",
+"    Posts about %s": "Αναρτήσεις για %s",
+"    Posts for year %s": "Αναρτήσεις για τη χρονιά %s",
+"    Previous post": "Προηγούμενη ανάρτηση",
+"    Read in English": "Διαβάστε στα Ελληνικά",
+"    Read more": "Read more",
+"    Source": "Source",
+"    Tags": "Ετικέτες",
+"    old posts page %d": "σελίδα παλαιότερων αναρτήσεων %d",
+}
