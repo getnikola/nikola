593a170c402803ae2492538e93f8645c5b112ee9
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-03-18 14:14:56 -0300

    removed obsolete test

--- tests/test_plugin_importing.py
+++ tests/test_plugin_importing.py
@@ -9,9 +9,6 @@ class ImportPluginsTest(unittest.TestCase):
     def test_importing_command_import_wordpress(self):
         import nikola.plugins.command_import_wordpress  # NOQA
 
-    def test_importing_task_sitemap(self):
-        import nikola.plugins.task_sitemap.sitemap_gen  # NOQA
-
     def test_importing_compile_rest(self):
         import nikola.plugins.compile_rest  # NOQA
 
