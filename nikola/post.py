0d777107708223f3a4c3c9149207cbc444f49740
Kay Hayen <kay.hayen@gmail.com>
2013-04-15 08:25:23 +0200

    lang fixup for master, adds "retired" support
    Hello,
    
    I was getting KeyErrors after rebasing to master. I believe it's because
    of a typo. The loop variable is used quoted, with kind of looks wrong.
    
    Check it out, and I also submit my support for "retired" posts. Please
    find the patch attached.
    
    I am using this to mark posts that were only temporary true, and are no
    longer of interest, to not appear anywhere in feeds. It won't give 404
    to people who still use it though (comments, links from outside, etc.)
    
    It probably would be nice to autmatically add a remark about that
    status. But that's future work.
    
    Yours,
    Kay
    
    --
    You received this message because you are subscribed to the Google Groups "nikola-discuss" group.
    To unsubscribe from this group and stop receiving emails from it, send an email to nikola-discuss+unsubscribe@googlegroups.com.
    For more options, visit https://groups.google.com/groups/opt_out.
    
    From 53cd577e46b0f31af60836973d6aeb2eeaef9b1f Mon Sep 17 00:00:00 2001
    From: Kay Hayen <kay.hayen@gmail.com>
    Date: Sat, 30 Mar 2013 10:00:46 +0100
    Subject: [PATCH] Added support for retired posts

--- nikola/post.py
+++ nikola/post.py
@@ -112,17 +112,21 @@ class Post(object):
         self.date = to_datetime(self.meta[default_lang]['date'], tzinfo)
 
         is_draft = False
+        is_retired = False
         self._tags = {}
         for lang in self.translated_to:
             self._tags[lang] = [x.strip() for x in self.meta[lang]['tags'].split(',')]
             self._tags[lang] = [t for t in self._tags[lang] if t]
             if 'draft' in self._tags[lang]:
                 is_draft = True
-                self._tags['lang'].remove('draft')
+                self._tags[lang].remove('draft')
+            if 'retired' in self._tags[lang]:
+                is_retired = True
+                self._tags[lang].remove('retired')
 
         # While draft comes from the tags, it's not really a tag
-        self.use_in_feeds = use_in_feeds and not is_draft
         self.is_draft = is_draft
+        self.use_in_feeds = use_in_feeds and not is_draft and not is_retired
 
         # If mathjax is a tag, then enable mathjax rendering support
         self.is_mathjax = 'mathjax' in self.tags
