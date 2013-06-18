af8b6620b752b4752b7245bc818c710a9132e0fe
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-06-17 22:09:15 -0300

    Added version command, Fix #504

---
+++ nikola/plugins/command_version.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2012 Roberto Alsina y otros.
+
+# Permission is hereby granted, free of charge, to any
+# person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the
+# Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the
+# Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice
+# shall be included in all copies or substantial portions of
+# the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+# KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+# OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+from __future__ import print_function
+import os
+import shutil
+import codecs
+
+from mako.template import Template
+
+import nikola
+from nikola.plugin_categories import Command
+from nikola.main import VERSION
+
+class CommandVersion(Command):
+    """Print the version."""
+
+    name = "version"
+
+    doc_usage = ""
+    needs_config = False
+    doc_purpose = """Print the Nikola version number."""
+
+    def _execute(self, options={}, args=None):
+        """Print the version number."""
+        print("Nikola version %s" % VERSION)
