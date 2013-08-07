fde836f9b54d6d657ffbfb3b1b7e5cd9803f6a3c
Areski Belaid <areski@gmail.com>
2013-08-07 18:53:20 +0200

    import themes for bootstrap3 - work in process

---
+++ nikola/data/themes/bootstrap3/messages/messages_pt_br.py
@@ -0,0 +1,23 @@
+# -*- encoding:utf-8 -*-
+from __future__ import unicode_literals
+
+MESSAGES = {
+    "Also available in": "Também disponível em",
+    "Archive": "Arquivo",
+    "LANGUAGE": "Português",
+    "More posts about": "Mais posts sobre",
+    "Newer posts": "Posts mais recentes",
+    "Next post": "Próximo post",
+    "Older posts": "Posts mais antigos",
+    "Original site": "Site original",
+    "Posted": "Publicado",
+    "Posts about %s": "Posts sobre %s",
+    "Posts for year %s": "Posts do ano %s",
+    "Posts for {month} {year}": "Posts de {month} {year}",
+    "Previous post": "Post anterior",
+    "Read in English": "Ler em português",
+    "Read more": "Leia mais",
+    "Source": "Código",
+    "Tags": "Tags",
+    "old posts page %d": "Posts antigos página %d",
+}
