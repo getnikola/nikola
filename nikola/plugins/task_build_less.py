a242f353cece7ee6be5afa9887554ed96dc99d17
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-08-10 00:28:43 -0300

    skeleton

---
+++ nikola/plugins/task_build_less.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © 2012-2013 Roberto Alsina and others.
+
+# Permission is hereby granted, free of charge, to any
+# person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the
+# Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the
+# Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice
+# shall be included in all copies or substantial portions of
+# the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+# KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+# OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+from __future__ import unicode_literals
+
+import os
+
+from nikola.plugin_categories import Task
+from nikola import utils
+
+
+class BuildLess(Task):
+    """Bundle assets using WebAssets."""
+
+    name = "build_less"
+
+    def gen_tasks(self):
+        """Generate CSS out of LESS sources."""
+
+        kw = {
+            'filters': self.site.config['FILTERS'],
+            'output_folder': self.site.config['OUTPUT_FOLDER'],
+            'cache_folder': self.site.config['CACHE_FOLDER'],
+            'theme_bundles': get_theme_bundles(self.site.THEMES),
+            'themes': self.site.THEMES,
+            'files_folders': self.site.config['FILES_FOLDERS'],
+            'code_color_scheme': self.site.config['CODE_COLOR_SCHEME'],
+        }
+        
+        # Find all LESS sources in theme/less/*.less
+        
+        # Generate theme/assets/css/*.css
+
+        
