5438f68360f8b0e53b48e32bbcc4f1b8b08ff974
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-08-20 15:19:38 -0300

    revert default behaviour of social buttons

--- nikola/nikola.py
+++ nikola/nikola.py
@@ -100,7 +100,7 @@ class Nikola(object):
         self.config = {
             'ADD_THIS_BUTTONS': True,
             'ANALYTICS': '',
-            'SOCIAL_BUTTONS_CODE': '',
+            'SOCIAL_BUTTONS_CODE': SOCIAL_BUTTONS_CODE,
             'ARCHIVE_PATH': "",
             'ARCHIVE_FILENAME': "archive.html",
             'CACHE_FOLDER': 'cache',
@@ -875,3 +875,18 @@ def s_l(lang):
         print("WARNING: could not set locale to {0}."
               "This may cause some i18n features not to work.".format(lang))
     return ''
+
+
+SOCIAL_BUTTONS_CODE = """
+<!-- Social buttons -->
+<div id="addthisbox" class="addthis_toolbox addthis_peekaboo_style addthis_default_style addthis_label_style addthis_32x32_style">
+<a class="addthis_button_more">Share</a>
+<ul><li><a class="addthis_button_facebook"></a>
+<li><a class="addthis_button_google_plusone_share"></a>
+<li><a class="addthis_button_linkedin"></a>
+<li><a class="addthis_button_twitter"></a>
+</ul>
+</div>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4f7088a56bb93798"></script>
+<!-- End of social buttons -->
+"""
