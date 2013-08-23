e70e82d6f8d80bd2f1b3241f420bc264c4c4ae61
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-08-23 15:59:48 +0200

    modify no-pyphen warning; add pyphen to full reqs (via #602)
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- nikola/nikola.py
+++ nikola/nikola.py
@@ -189,7 +189,9 @@ class Nikola(object):
 
         # Make sure we have pyphen installed if we are using it
         if self.config.get('HYPHENATE') and pyphen is None:
-            print('WARNING: Hyphenation support requires pyphen, setting HYPHENATE to False')
+            print('WARNING: To use the hyphenation, you have to install '
+                  'the "pyphen" package.')
+            print('WARNING: Setting HYPHENATE to False.')
             self.config['HYPHENATE'] = False
 
         # Deprecating the ADD_THIS_BUTTONS option
