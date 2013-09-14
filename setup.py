afbe371921c0881366800e340e084823af8c988c
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-09-14 18:14:39 -0300

    preparing 6.0.3

--- setup.py
+++ setup.py
@@ -207,7 +207,7 @@ def find_package_data(
     return out
 
 setup(name='Nikola',
-      version='6.0.2',
+      version='6.0.3',
       description='Static blog/website generator',
       author='Roberto Alsina and others',
       author_email='ralsina@netmanagers.com.ar',
