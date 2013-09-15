e5452ac5031fee58302590d14581af1dde6f634d
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-09-14 21:07:21 -0300

    which is unixy, and check_call already fails if pandoc is not installed

--- nikola/plugins/compile/pandoc.py
+++ nikola/plugins/compile/pandoc.py
@@ -44,18 +44,17 @@ class CompilePandoc(PageCompiler):
 
     def compile_html(self, source, dest, is_two_file=True):
         try:
-            pandoc_path = subprocess.check_output(('which', 'pandoc')).splitlines()[0]
-        except subprocess.CalledProcessError:
-            print('To use the pandoc compiler,'
-                  ' you have to install the "pandoc" Haskell package.')
-            raise Exception('Cannot compile {0} -- pandoc '
-                            'missing'.format(source))
-
-        try:
             os.makedirs(os.path.dirname(dest))
         except:
             pass
-        subprocess.check_call((pandoc_path, '-o', dest, source))
+        try:
+            subprocess.check_call(('pandoc', '-o', dest, source))
+        except OSError as e:
+            if e.strreror == 'No such file or directory':
+                print('To use the pandoc compiler,'
+                      ' you have to install the "pandoc" Haskell package.')
+                raise Exception('Cannot compile {0} -- pandoc '
+                                'missing'.format(source))
 
     def create_post(self, path, onefile=False, **kw):
         metadata = {}
