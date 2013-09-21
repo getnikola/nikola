1d3ee45f285c8f88d9344111467f8daf5bce8122
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-09-21 00:43:43 -0300

    test monthly archive creation

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -313,5 +313,26 @@ class RelativeLinkTest2(DemoBuildTest):
         self.assertFalse('<loc>http://getnikola.com/</loc>' in sitemap_data)
         self.assertTrue('<loc>http://getnikola.com/blog/</loc>' in sitemap_data)
 
+
+class MonthlyArchiveTest(DemoBuildTest):
+    """Check that the monthly archives build and are correct."""
+
+    @classmethod
+    def patch_site(self):
+        """Set the SITE_URL to have a path"""
+        conf_path = os.path.join(self.target_dir, "conf.py")
+        with codecs.open(conf_path, "rb", "utf-8") as inf:
+            data = inf.read()
+            data = data.replace('# CREATE_MONTHLY_ARCHIVE = False',
+                                'CREATE_MONTHLY_ARCHIVE = True')
+        with codecs.open(conf_path, "wb+", "utf8") as outf:
+            outf.write(data)
+            outf.flush()
+
+    def test_monthly_archive(self):
+        """See that it builds"""
+        self.assertTrue(os.path.isfile(os.path.join(self.tmpdir, 'target', 'output', '2012', '03', 'index.html')))
+
+
 if __name__ == "__main__":
     unittest.main()
