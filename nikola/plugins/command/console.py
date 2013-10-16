3536030a41ac51357c134cd82aebf83b16dfe30d
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-10-16 16:39:56 +0200

    change arrows
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- nikola/plugins/command/console.py
+++ nikola/plugins/command/console.py
@@ -39,7 +39,7 @@ class Console(Command):
     """Start debugging console."""
     name = "console"
     shells = ['ipython', 'bpython', 'plain']
-    doc_purpose = "Start an interactive Python (IPython→bpython→plain) console with access to your site and configuration"
+    doc_purpose = "Start an interactive Python (IPython->bpython->plain) console with access to your site and configuration"
     header = "Nikola v" + __version__ + " -- {0} Console (conf = configuration, SITE = site engine)"
 
     def ipython(self):
