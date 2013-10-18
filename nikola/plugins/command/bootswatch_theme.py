d600232ad6a8b5ec06530b3df4376fdb398cb98b
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-10-18 19:39:27 +0200

    if `check` gets to have such logs, then why `bootswatch_theme` doesn’t?

--- nikola/plugins/command/bootswatch_theme.py
+++ nikola/plugins/command/bootswatch_theme.py
@@ -35,8 +35,6 @@ except ImportError:
 from nikola.plugin_categories import Command
 from nikola import utils
 
-LOGGER = utils.get_logger('bootswatch_theme', utils.STDERR_HANDLER)
-
 
 class CommandBootswatchTheme(Command):
     """Given a swatch name from bootswatch.com and a parent theme, creates a custom theme."""
@@ -79,19 +77,21 @@ class CommandBootswatchTheme(Command):
         swatch = options['swatch']
         parent = options['parent']
         version = ''
+        
+        self.logger = utils.get_logger('bootswatch_theme', self.site.loghandlers)
 
         # See if we need bootswatch for bootstrap v2 or v3
         themes = utils.get_theme_chain(parent)
         if 'bootstrap3' not in themes:
             version = '2'
         elif 'bootstrap' not in themes:
-            LOGGER.warn('"bootswatch_theme" only makes sense for themes that use bootstrap')
+            self.logger.warn('"bootswatch_theme" only makes sense for themes that use bootstrap')
 
-        LOGGER.notice("Creating '{0}' theme from '{1}' and '{2}'".format(name, swatch, parent))
+        self.logger.notice("Creating '{0}' theme from '{1}' and '{2}'".format(name, swatch, parent))
         utils.makedirs(os.path.join('themes', name, 'assets', 'css'))
         for fname in ('bootstrap.min.css', 'bootstrap.css'):
             url = '/'.join(('http://bootswatch.com', version, swatch, fname))
-            LOGGER.notice("Downloading: " + url)
+            self.logger.notice("Downloading: " + url)
             data = requests.get(url).text
             with open(os.path.join('themes', name, 'assets', 'css', fname),
                       'wb+') as output:
@@ -99,5 +99,5 @@ class CommandBootswatchTheme(Command):
 
         with open(os.path.join('themes', name, 'parent'), 'wb+') as output:
             output.write(parent.encode('utf-8'))
-        LOGGER.notice('Theme created. Change the THEME setting to "{0}" to use '
+        self.logger.notice('Theme created. Change the THEME setting to "{0}" to use '
                       'it.'.format(name))
