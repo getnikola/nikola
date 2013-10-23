4a0fce541a0b7afbe6d57d1b1084e422260c460f
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-10-23 15:07:43 +0200

    Reverse yesterday’s path changes

--- nikola/post.py
+++ nikola/post.py
@@ -105,9 +105,9 @@ class Post(object):
         self.source_path = source_path  # posts/blah.txt
         self.post_name = os.path.splitext(source_path)[0]  # posts/blah
         # cache[\/]posts[\/]blah.html
-        self._base_path = os.path.join(self.config['CACHE_FOLDER'], self.post_name + ".html")
+        self.base_path = os.path.join(self.config['CACHE_FOLDER'], self.post_name + ".html")
         # cache/posts/blah.html
-        self.base_path = self._base_path.replace('\\', '/')
+        self._base_path = self._base_path.replace('\\', '/')
         self.metadata_path = self.post_name + ".meta"  # posts/blah.meta
         self.folder = destination
         self.translations = self.config['TRANSLATIONS']
