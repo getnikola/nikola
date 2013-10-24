4ce3047189eac6d36497ff966091beda54fa54a6
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-10-24 10:48:54 -0300

    use sphinxcontrib.gist if it's installed

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -27,7 +27,11 @@ from __future__ import unicode_literals
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+try:
+    import sphinxcontrib.gist
+    extensions = ['sphinxcontrib.gist']
+except ImportError:
+    extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
