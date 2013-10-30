70a37153d5bd41eeaef2096a6f517884ac453b9d
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-10-30 16:48:28 +0100

    Optimize the test suite.
    Changes include:
    
     * default tests locale is Polish instead of Spanish
    
       Rationale: see commit 049f59a.
    
                  tl¹;dr: 1 vs 20 locales to generate, that’s all.
    
     * flake8 is running on its own build jobs.
    
       Rationale: we are now able to tell what passed and what didn’t at a
                  glance.  We do not have to search through stuff to find out
                  what (nosetests or flake8) failed.
    
     * Use the Travis apt cache, which will save us a few seconds here or
       there.
    
    ¹ This commit message is actually longer than the one in 049f59a.
    
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- tests/base.py
+++ tests/base.py
@@ -106,18 +106,18 @@ class LocaleSupportInTesting(object):
         if hasattr(cls, 'langlocales'):
             return
         defaults = {
-            'linux': {
+            'posix': {
                 # non-windows defaults, must be two locales suported by .travis.yml
                 'default': ("en", str("en_US.utf8")),
-                'other': ("es", str("es_ES.utf8")),
+                'other': ("pl", str("pl_PL.utf8")),
             },
             'windows': {
                 # windows defaults
                 'default': ("en", str("English")),
-                'other': ("es", str("Spanish")),
+                'other': ("pl", str("Polish")),
             },
         }
-        os_id = 'windows' if sys.platform == 'win32' else 'linux'
+        os_id = 'windows' if sys.platform == 'win32' else 'posix'
         langlocales = {}
         for suffix in ['other', 'default']:
             try:
