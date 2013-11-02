f00bdd68c6da335b3cffd66d2950cf3b5ee13c94
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-11-02 08:55:46 +0100

    flake8
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- nikola/post.py
+++ nikola/post.py
@@ -144,8 +144,7 @@ class Post(object):
         if 'date' not in default_metadata and not use_in_feeds:
             # For stories we don't *really* need a date
             default_metadata['date'] = datetime.datetime.utcfromtimestamp(
-                os.stat(self.source_path).st_ctime).replace(
-                    tzinfo=pytz.UTC).astimezone(tzinfo)
+                os.stat(self.source_path).st_ctime).replace(tzinfo=pytz.UTC).astimezone(tzinfo)
 
         if 'title' not in default_metadata or 'slug' not in default_metadata \
                 or 'date' not in default_metadata:
