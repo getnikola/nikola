43ae9d51ea93b8c4e45382f14d504b9ba2322aa3
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2014-02-01 10:33:22 +0100

    oh what.  zips created by github do not have a `v` in the output directory name.
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- scripts/getpyver.py
+++ scripts/getpyver.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 # For internal use only.
-"""Return the Python version in a sane format (vX.Y.Z)."""
+"""Return the Python version in a sane format (vX.Y.Z).
+
+Also available a less sane format (X.Y.Z) if `short` is provided
+as an argument.
+
+"""
 import sys
-print("v" + (".".join([str(i) for i in sys.version_info[0:3]])))
+if 'short' in sys.argv:
+    print(".".join([str(i) for i in sys.version_info[0:3]]))
+else:
+    print("v" + (".".join([str(i) for i in sys.version_info[0:3]])))
