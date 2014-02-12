601dec18c13b78b950b719cc160a8d3b4c8626ac
Aaron Meurer <asmeurer@gmail.com>
2014-02-11 19:24:02 -0600

    Colorize the handler from nikola/plugins/loghandler/stderr.py

--- nikola/plugins/loghandler/stderr.py
+++ nikola/plugins/loghandler/stderr.py
@@ -40,7 +40,7 @@ class StderrHandler(SignalHandler):
         """Attach the handler to the logger."""
         conf = self.site.config.get('LOGGING_HANDLERS').get('stderr')
         if conf or os.getenv('NIKOLA_DEBUG'):
-            self.site.loghandlers.append(logbook.StderrHandler(
+            self.site.loghandlers.append(logbook.more.ColorizedStderrHandler(
                 level='DEBUG' if DEBUG else conf.get('loglevel', 'WARNING').upper(),
                 format_string=u'[{record.time:%Y-%m-%dT%H:%M:%SZ}] {record.level_name}: {record.channel}: {record.message}'
             ))
