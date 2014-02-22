1b19f03d1027ca8088a67ef56ade4fe721bb659a
Thibauld Nion <thibauld@tibonihoo.net>
2014-02-22 19:26:54 +0100

    put default translation pattern in an easy to recover global variable

--- nikola/nikola.py
+++ nikola/nikola.py
@@ -75,6 +75,10 @@ config_changed = utils.config_changed
 
 __all__ = ['Nikola']
 
+# Default pattern for translation files' names
+# WARNING: it is also hard-coded in conf.py.in
+DEFAULT_TRANSLATIONS_PATTERN = '{path}.{ext}.{lang}'
+
 
 class Nikola(object):
 
@@ -88,7 +92,7 @@ class Nikola(object):
         'local_search',
         'render_mustache',
     ]
-
+    
     def __init__(self, **config):
         """Setup proper environment for running tasks."""
 
@@ -232,9 +236,9 @@ class Nikola(object):
             'SCHEDULE_FORCE_TODAY': False,
             'LOGGING_HANDLERS': {'stderr': {'loglevel': 'WARNING', 'bubble': True}},
             'DEMOTE_HEADERS': 1,
-            'TRANSLATIONS_PATTERN': '{path}.{ext}.{lang}',
+            'TRANSLATIONS_PATTERN': DEFAULT_TRANSLATIONS_PATTERN,
         }
-
+        
         self.config.update(config)
 
         # Translatability configuration.
