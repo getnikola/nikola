3e8bfed86b93e759294a5f3a52ed4b9079792f34
Daniel Aleksandersen <code@daniel.priv.no>
2014-03-01 00:46:31 +0100

    Remove unused literal_eval

--- nikola/plugins/command/deploy.py
+++ nikola/plugins/command/deploy.py
@@ -25,7 +25,6 @@
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 from __future__ import print_function
-from ast import literal_eval
 import codecs
 from datetime import datetime
 import os
