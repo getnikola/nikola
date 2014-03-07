ed2179593fd1f233e11fd05de1df0a9c2199d149
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2014-03-07 19:33:37 +0100

    Set version to 7.0.0-alpha
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- nikola/__init__.py
+++ nikola/__init__.py
@@ -27,7 +27,7 @@
 from __future__ import absolute_import
 import os
 
-__version__ = "6.9.90"
+__version__ = "7.0.0-alpha"
 DEBUG = bool(os.getenv('NIKOLA_DEBUG'))
 
 from .nikola import Nikola  # NOQA
