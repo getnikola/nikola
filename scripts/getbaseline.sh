b4818a1303130d8a852ec2717b40f7829a94d05e
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2014-03-19 15:51:52 +0100

    missing `short` parameter for getpyver.py
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- scripts/getbaseline.sh
+++ scripts/getbaseline.sh
@@ -3,11 +3,12 @@ cd tests/data
 for i in $@; do
     if [[ $i == '2.7' ]]; then
         # we only support 2.7 now
-        echo 'Version '$i' does not have baseline testing.'
         wget https://github.com/getnikola/invariant-builds/archive/v$i'.zip'
         unzip 'v'$i'.zip'
         rm -rf baseline$i
         mv invariant-builds-$i baseline$i
         rm 'v'$i'.zip'
+    else
+        echo 'Version '$i' does not support baseline testing.'
     fi
 done
