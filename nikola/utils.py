190295b288f4f142b7894d6d2cc8708084dc6e42
Roberto Alsina <ralsina@netmanagers.com.ar>
2014-03-25 18:57:00 -0300

    flake8

--- nikola/utils.py
+++ nikola/utils.py
@@ -637,13 +637,13 @@ def to_datetime(value, tzinfo=None):
             # so revert them
             if isinstance(dt.tzinfo, dateutil.tz.tzoffset):
                 offset = dt.tzinfo.utcoffset(dt)
-                seconds = offset.days*24*3600 + offset.seconds
+                seconds = offset.days * 24 * 3600 + offset.seconds
                 dt = dt.replace(tzinfo=dateutil.tz.tzoffset(None, -seconds))
             dt = dt.astimezone(tzinfo)
         else:
             dt.replace(tzinfo=tzinfo)
         return dt
-    except Exception as exc:
+    except Exception:
         raise ValueError('Unrecognized date/time: {0!r}'.format(value))
 
 
