b682aa4a74db2e673c82d4d1e0ae29d86a438603
Roberto Alsina <ralsina@netmanagers.com.ar>
2014-03-28 10:59:27 -0300

    missed that spot

--- nikola/plugins/compile/html.py
+++ nikola/plugins/compile/html.py
@@ -56,7 +56,7 @@ class CompileHtml(PageCompiler):
             out_file.write(data)
         return True
 
-    def create_post(self, path, content, onefile=False, is_page=False, **kw):
+    def create_post(self, path, **kw):
         content = kw.pop('content', None)
         one_file = kw.pop('one_file', False)
         is_page = kw.pop('is_page', False)
