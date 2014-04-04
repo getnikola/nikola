a08ddaac08e7c9b6b67b52f8fb32ddf6647e1c50
Roberto Alsina <ralsina@netmanagers.com.ar>
2014-04-04 09:46:05 -0300

    flake8

--- nikola/utils.py
+++ nikola/utils.py
@@ -405,7 +405,7 @@ class config_changed(tools.config_changed):
             else:
                 byte_data = data
             digest = hashlib.md5(byte_data).hexdigest()
-            #LOGGER.debug('{0}: {1}'.format(digest, byte_data))
+            # LOGGER.debug('{0}: {1}'.format(digest, byte_data))
             return digest
         else:
             raise Exception('Invalid type of config_changed parameter -- got '
