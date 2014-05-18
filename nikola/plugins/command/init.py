f7cdd87fafd34ffa33e4c88eca642aaf43b9c2a5
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2014-05-18 17:45:55 +0200

    add unicode_literals future-import (via #1288)
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- nikola/plugins/command/init.py
+++ nikola/plugins/command/init.py
@@ -24,7 +24,7 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from __future__ import print_function
+from __future__ import print_function, unicode_literals
 import os
 import shutil
 import codecs
