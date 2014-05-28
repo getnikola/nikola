6644e3be19c623f1fc2cfbad65c23a224ea0e2ac
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2014-05-28 18:11:46 +0200

    fix list generator
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- scripts/generate_symlinked_list.sh
+++ scripts/generate_symlinked_list.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-# generates a list of symlinked files and directories.
-# each line must be the file path relative to the git root 
+# Generate a list of symlinked files and directories.
+# Each line must be the file path, relative to the git root.
+
 WDir="${PWD##*/}"
-if [ $WDir != scripts ]; then
-	echo "Error, Script must be run from scripts directory. Nothing done."
-	exit 1
-fi	
-cd ..
+[[ $WDir == 'scripts' ]] && cd ..
+
 dst='nikola/data/symlinked.txt'
 git ls-files -s | awk '/120000/{print $4}' > $dst
