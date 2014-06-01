e2df1fe5f06bbe5b283fd8ba296fe65d7b766e92
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2014-06-01 18:23:24 +0200

    remove some newlines
    [ci skip]
    
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- nikola/plugins/command/github_deploy.py
+++ nikola/plugins/command/github_deploy.py
@@ -214,18 +214,15 @@ class CommandGitHubDeploy(Command):
 
         try:
             remotes = subprocess.check_output(['git', 'remote'])
-
         except subprocess.CalledProcessError as e:
             self.logger.notice('github_deploy needs a git repository!')
             sys.exit(e.returncode)
-
         except OSError as e:
             import errno
             self.logger.error('Running git failed with {0}'.format(e))
             if e.errno == errno.ENOENT:
                 self.logger.notice('Is git on the PATH?')
             sys.exit(1)
-
         else:
             if self._remote_name not in remotes:
                 self.logger.error(
@@ -263,7 +260,6 @@ class CommandGitHubDeploy(Command):
                 "or committed onto the wrong branch. Do you wish to continue?"
             ) % changes
             proceed = ask_yesno(message, False)
-
         else:
             proceed = True
 
