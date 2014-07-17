13a8788d112871f00f6053aa7204602519694550
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2014-07-17 14:41:08 +0200

    translations
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

---
+++ nikola/data/themes/base/messages/messages_gl.py
@@ -0,0 +1,33 @@
+# -*- encoding:utf-8 -*-
+from __future__ import unicode_literals
+
+MESSAGES = {
+    "%d min remaining to read": "",
+    "Also available in:": "",
+    "Archive": "",
+    "Categories": "",
+    "Comments": "",
+    "LANGUAGE": "",
+    "Languages:": "",
+    "More posts about %s": "",
+    "Newer posts": "",
+    "Next post": "",
+    "No posts found.": "",
+    "Nothing found.": "",
+    "Older posts": "",
+    "Original site": "",
+    "Posted:": "",
+    "Posts about %s": "",
+    "Posts for year %s": "",
+    "Posts for {month} {year}": "",
+    "Previous post": "",
+    "Publication date": "",
+    "RSS feed": "",
+    "Read in English": "",
+    "Read more": "",
+    "Source": "",
+    "Tags and Categories": "",
+    "Tags": "",
+    "old posts, page %d": "",
+    "page %d": "",
+}
