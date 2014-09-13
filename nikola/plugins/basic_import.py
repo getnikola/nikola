dd79328fd0b80aa40afaa8da575bad8ff73db0fc
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2014-09-13 13:34:48 +0200

    simplify #1416 fix
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- nikola/plugins/basic_import.py
+++ nikola/plugins/basic_import.py
@@ -140,16 +140,11 @@ class ImportMixin(object):
     @staticmethod
     def write_urlmap_csv(output_file, url_map):
         utils.makedirs(os.path.dirname(output_file))
-        if sys.version_info[0] == 2:
-            with io.open(output_file, 'wb+') as fd:
-                csv_writer = csv.writer(fd)
-                for item in url_map.items():
-                    csv_writer.writerow(item)
-        else:
-            with open(output_file, 'w+') as fd:
-                csv_writer = csv.writer(fd)
-                for item in url_map.items():
-                    csv_writer.writerow(item)
+        fmode = 'wb+' if sys.version_info[0] == 2 else 'w+'
+        with io.open(output_file, fmode) as fd:
+            csv_writer = csv.writer(fd)
+            for item in url_map.items():
+                csv_writer.writerow(item)
 
     def get_configuration_output_path(self):
         if not self.import_into_existing_site:
