61e65afd494e995319e8f6766ac394a8b7cac7e9
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2014-10-06 18:31:29 +0200

    never rebase in github_deploy (via #1401)
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- nikola/plugins/command/github_deploy.py
+++ nikola/plugins/command/github_deploy.py
@@ -135,10 +135,10 @@ class CommandGitHubDeploy(Command):
         )
 
         commands = [
-            ['git', 'pull', remote, '%s:%s' % (deploy, deploy)],
+            ['git', 'pull', '--rebase=false', remote, '%s:%s' % (deploy, deploy)],
             ['git', 'add', '-A'],
             ['git', 'commit', '-m', commit_message],
-            ['git', 'push', remote, '%s:%s' % (deploy, deploy)],
+            ['git', 'push', '--force', remote, '%s:%s' % (deploy, deploy)],
             ['git', 'checkout', source],
         ]
 
