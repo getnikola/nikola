41b0e4bfddd3db70f202ab2c0b109257b0721c41
Felix Fontein <felix@fontein.de>
2014-11-23 17:20:54 +0100

    Added support for new option LISTINGS_FOLDERS.

--- nikola/plugins/task/listings.py
+++ nikola/plugins/task/listings.py
@@ -55,17 +55,30 @@ class Listings(Task):
         """Render pretty code listings."""
         kw = {
             "default_lang": self.site.config["DEFAULT_LANG"],
-            "listings_folder": self.site.config["LISTINGS_FOLDER"],
+            "listings_folders": self.site.config["LISTINGS_FOLDERS"],
             "output_folder": self.site.config["OUTPUT_FOLDER"],
             "index_file": self.site.config["INDEX_FILE"],
             "strip_indexes": self.site.config['STRIP_INDEXES'],
             "filters": self.site.config["FILTERS"],
         }
 
+        # Verify that no folder in LISTINGS_FOLDERS appears twice (neither on input nor output side)
+        self.input_folders = set()
+        self.output_folders = set()
+        for input_folder, output_folder in kw['listings_folders'].items():
+            if input_folder in self.input_folders:
+                utils.LOGGER.error("Listings input folder '{0}' specified more than once!".format(input_folder))
+                raise Exception("Listings input folder '{0}' specified more than once!".format(input_folder))
+            if output_folder in self.output_folders:
+                utils.LOGGER.error("Listings output folder '{0}' specified more than once!".format(output_folder))
+                raise Exception("Listings output folder '{0}' specified more than once!".format(output_folder))
+            self.input_folders.add(input_folder)
+            self.output_folders.add(output_folder)
+
         # Things to ignore in listings
         ignored_extensions = (".pyc", ".pyo")
 
-        def render_listing(in_name, out_name, folders=[], files=[]):
+        def render_listing(in_name, out_name, input_folder, output_folder, folders=[], files=[]):
             if in_name:
                 with open(in_name, 'r') as fd:
                     try:
@@ -89,11 +102,13 @@ class Listings(Task):
                                       is_file=True)
             permalink = self.site.link(
                 'listing',
-                os.path.relpath(
-                    out_name,
-                    os.path.join(
-                        kw['output_folder'],
-                        kw['listings_folder'])))
+                os.path.join(
+                    input_folder,
+                    os.path.relpath(
+                        out_name,
+                        os.path.join(
+                            kw['output_folder'], 
+                            output_folder))))
             if self.site.config['COPY_SOURCES']:
                 source_link = permalink[:-5]
             else:
@@ -114,76 +129,87 @@ class Listings(Task):
 
         yield self.group_task()
 
+        self.improper_input_file_mapping = dict()
+        self.proper_input_file_mapping = dict()
         template_deps = self.site.template_system.template_deps('listing.tmpl')
-        for root, dirs, files in os.walk(kw['listings_folder'], followlinks=True):
-            files = [f for f in files if os.path.splitext(f)[-1] not in ignored_extensions]
+        for input_folder, output_folder in kw['listings_folders'].items():
+            for root, dirs, files in os.walk(input_folder, followlinks=True):
+                files = [f for f in files if os.path.splitext(f)[-1] not in ignored_extensions]
 
-            uptodate = {'c': self.site.GLOBAL_CONTEXT}
+                uptodate = {'c': self.site.GLOBAL_CONTEXT}
 
-            for k, v in self.site.GLOBAL_CONTEXT['template_hooks'].items():
-                uptodate['||template_hooks|{0}||'.format(k)] = v._items
+                for k, v in self.site.GLOBAL_CONTEXT['template_hooks'].items():
+                    uptodate['||template_hooks|{0}||'.format(k)] = v._items
 
-            for k in self.site._GLOBAL_CONTEXT_TRANSLATABLE:
-                uptodate[k] = self.site.GLOBAL_CONTEXT[k](kw['default_lang'])
+                for k in self.site._GLOBAL_CONTEXT_TRANSLATABLE:
+                    uptodate[k] = self.site.GLOBAL_CONTEXT[k](kw['default_lang'])
 
-            # save navigation links as dependencies
-            uptodate['navigation_links'] = uptodate['c']['navigation_links'](kw['default_lang'])
+                # save navigation links as dependencies
+                uptodate['navigation_links'] = uptodate['c']['navigation_links'](kw['default_lang'])
 
-            uptodate['kw'] = kw
+                uptodate['kw'] = kw
 
-            uptodate2 = uptodate.copy()
-            uptodate2['f'] = files
-            uptodate2['d'] = dirs
+                uptodate2 = uptodate.copy()
+                uptodate2['f'] = files
+                uptodate2['d'] = dirs
 
-            # Render all files
-            out_name = os.path.join(
-                kw['output_folder'],
-                root, kw['index_file']
-            )
-            yield utils.apply_filters({
-                'basename': self.name,
-                'name': out_name,
-                'file_dep': template_deps,
-                'targets': [out_name],
-                'actions': [(render_listing, [None, out_name, dirs, files])],
-                # This is necessary to reflect changes in blog title,
-                # sidebar links, etc.
-                'uptodate': [utils.config_changed(uptodate2)],
-                'clean': True,
-            }, kw["filters"])
-            for f in files:
-                ext = os.path.splitext(f)[-1]
-                if ext in ignored_extensions:
-                    continue
-                in_name = os.path.join(root, f)
-                out_name = os.path.join(
-                    kw['output_folder'],
-                    root,
-                    f) + '.html'
+                # Compute relative path; can't use os.path.relpath() here as it
+                rel_path = root[len(input_folder):] # returns "." instead of ""
+                if rel_path[:1] == os.sep:
+                    rel_path = rel_path[1:]
+
+                # Record file names
+                rel_name = os.path.join(rel_path, kw['index_file'])
+                rel_output_name = os.path.join(output_folder, rel_path, kw['index_file'])
+                self.register_output_name(input_folder, rel_name, rel_output_name)
+
+                # Render all files
+                out_name = os.path.join(kw['output_folder'], rel_output_name)
                 yield utils.apply_filters({
                     'basename': self.name,
                     'name': out_name,
-                    'file_dep': template_deps + [in_name],
+                    'file_dep': template_deps,
                     'targets': [out_name],
-                    'actions': [(render_listing, [in_name, out_name])],
+                    'actions': [(render_listing, [None, out_name, dirs, files])],
                     # This is necessary to reflect changes in blog title,
                     # sidebar links, etc.
-                    'uptodate': [utils.config_changed(uptodate)],
+                    'uptodate': [utils.config_changed(uptodate2)],
                     'clean': True,
                 }, kw["filters"])
-                if self.site.config['COPY_SOURCES']:
-                    out_name = os.path.join(
-                        kw['output_folder'],
-                        root,
-                        f)
+                for f in files:
+                    ext = os.path.splitext(f)[-1]
+                    if ext in ignored_extensions:
+                        continue
+                    in_name = os.path.join(root, f)
+                    # Record file names
+                    rel_name = os.path.join(rel_path, f + '.html')
+                    rel_output_name = os.path.join(output_folder, rel_path, f + '.html')
+                    self.register_output_name(input_folder, rel_name, rel_output_name)
+                    # Set up output name
+                    out_name = os.path.join(kw['output_folder'], rel_output_name)
+                    # Yield task
                     yield utils.apply_filters({
                         'basename': self.name,
                         'name': out_name,
-                        'file_dep': [in_name],
+                        'file_dep': template_deps + [in_name],
                         'targets': [out_name],
-                        'actions': [(utils.copy_file, [in_name, out_name])],
+                        'actions': [(render_listing, [in_name, out_name])],
+                        # This is necessary to reflect changes in blog title,
+                        # sidebar links, etc.
+                        'uptodate': [utils.config_changed(uptodate)],
                         'clean': True,
                     }, kw["filters"])
+                    if self.site.config['COPY_SOURCES']:
+                        rel_output_name = os.path.join(output_folder, rel_path, f)
+                        out_name = os.path.join(kw['output_folder'], rel_output_name)
+                        yield utils.apply_filters({
+                            'basename': self.name,
+                            'name': out_name,
+                            'file_dep': [in_name],
+                            'targets': [out_name],
+                            'actions': [(utils.copy_file, [in_name, out_name])],
+                            'clean': True,
+                        }, kw["filters"])
 
     def listing_path(self, name, lang):
         if not name.endswith('.html'):
