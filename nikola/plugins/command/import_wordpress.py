a9b2e6be95f0601a8a70a683a54a274cbc04e914
Chris Warrick <kwpolska@gmail.com>
2014-11-23 17:33:17 +0100

    remove _glue_xml_lines for performance reasons
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- nikola/plugins/command/import_wordpress.py
+++ nikola/plugins/command/import_wordpress.py
@@ -186,26 +186,6 @@ class CommandImportWordpress(Command, ImportMixin):
                                  rendered_template)
 
     @classmethod
-    def _glue_xml_lines(cls, xml):
-        new_xml = xml[0]
-        previous_line_ended_in_newline = new_xml.endswith(b'\n')
-        previous_line_was_indentet = False
-        for line in xml[1:]:
-            if (re.match(b'^[ \t]+', line) and previous_line_ended_in_newline):
-                new_xml = b''.join((new_xml, line))
-                previous_line_was_indentet = True
-            elif previous_line_was_indentet:
-                new_xml = b''.join((new_xml, line))
-                previous_line_was_indentet = False
-            else:
-                new_xml = b'\n'.join((new_xml, line))
-                previous_line_was_indentet = False
-
-            previous_line_ended_in_newline = line.endswith(b'\n')
-
-        return new_xml
-
-    @classmethod
     def read_xml_file(cls, filename):
         xml = []
 
@@ -215,8 +195,7 @@ class CommandImportWordpress(Command, ImportMixin):
                 if b'<atom:link rel=' in line:
                     continue
                 xml.append(line)
-
-        return cls._glue_xml_lines(xml)
+        return b'\n'.join(xml)
 
     @classmethod
     def get_channel_from_file(cls, filename):
