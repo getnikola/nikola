a16b3d5376a27d412569bb572729e0d4e0de6c90
Pelle Nilsson <pellenilsson@fastmail.fm>
2014-11-29 21:49:59 +0100

    Thumbnail directive can imitate Figure as well as Image directives

--- nikola/plugins/compile/rest/thumbnail.py
+++ nikola/plugins/compile/rest/thumbnail.py
@@ -1,7 +1,7 @@
 import os
 
 from docutils.parsers.rst import Directive, directives
-from docutils.parsers.rst.directives.images import Image
+from docutils.parsers.rst.directives.images import Image, Figure
 
 from nikola.plugin_categories import RestExtension
 
@@ -15,14 +15,28 @@ class Plugin(RestExtension):
         return super(Plugin, self).set_site(site)
 
 
-class Thumbnail(Image):
+class Thumbnail(Figure):
+
+    def align(argument):
+        return directives.choice(argument, Image.align_values)
+
+    def figwidth_value(argument):
+        if argument.lower() == 'image':
+            return 'image'
+        else:
+            return directives.length_or_percentage_or_unitless(argument, 'px')
 
     option_spec = Image.option_spec.copy()
+    option_spec['figwidth'] = figwidth_value
+    option_spec['figclass'] = directives.class_option
+    has_content = True
 
     def run(self):
         uri = directives.uri(self.arguments[0])
         self.options['target'] = uri
         self.arguments[0] = '.thumbnail'.join(os.path.splitext(uri))
-        (image_node,) = Image.run(self)
-        return [image_node]
-
+        if self.content:
+            (node,) = Figure.run(self)
+        else:
+            (node,) = Image.run(self)
+        return [node]
