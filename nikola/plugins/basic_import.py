49c2699477c9640cc30e61e8371048d9d5007119
Chris Warrick <kwpolska@gmail.com>
2014-11-30 11:07:55 +0100

    use new-style metadata in importers
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- nikola/plugins/basic_import.py
+++ nikola/plugins/basic_import.py
@@ -130,12 +130,7 @@ class ImportMixin(object):
 
         utils.makedirs(os.path.dirname(filename))
         with io.open(filename, "w+", encoding="utf8") as fd:
-            fd.write('{0}\n'.format(title))
-            fd.write('{0}\n'.format(slug))
-            fd.write('{0}\n'.format(post_date))
-            fd.write('{0}\n'.format(','.join(tags)))
-            fd.write('\n')
-            fd.write('{0}\n'.format(description))
+            fd.write(utils.write_metadata({'title': title, 'slug': slug, 'date': post_date, 'tags': tags, 'description': description}))
 
     @staticmethod
     def write_urlmap_csv(output_file, url_map):
