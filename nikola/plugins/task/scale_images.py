e35b80819fa6f99f93b579a0b6ee3a85ca5bfa68
Pelle Nilsson <pellenilsson@fastmail.fm>
2014-12-07 19:14:45 +0100

    Add copyright notice to thumbnail and scale_images plugins

--- nikola/plugins/task/scale_images.py
+++ nikola/plugins/task/scale_images.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright © 2012-2014 Roberto Alsina and others.
+# Copyright © 2014 Pelle Nilsson and others.
 
 # Permission is hereby granted, free of charge, to any
 # person obtaining a copy of this software and associated
