1705e9014871a102d374d06533739a873f2c7ed0
Chris Warrick <kwpolska@gmail.com>
2014-12-21 21:02:02 +0100

    Rename INDEXES_PAGES_STATIC to INDEXES_STATIC
    didn’t make sense; _PAGES refers to the (old posts page %d) ONLY.
    
    cc @felixfontein
    
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- nikola/nikola.py
+++ nikola/nikola.py
@@ -335,7 +335,7 @@ class Nikola(object):
             'INDEXES_TITLE': "",
             'INDEXES_PAGES': "",
             'INDEXES_PAGES_MAIN': False,
-            'INDEXES_PAGES_STATIC': True,
+            'INDEXES_STATIC': True,
             'INDEX_PATH': '',
             'IPYNB_CONFIG': {},
             'LESS_COMPILER': 'lessc',
@@ -1486,11 +1486,11 @@ class Nikola(object):
         kw["index_teasers"] = self.config['INDEX_TEASERS']
         kw["indexes_pages"] = self.config['INDEXES_PAGES']
         kw["indexes_pages_main"] = self.config['INDEXES_PAGES_MAIN']
-        kw["indexes_pages_static"] = self.config['INDEXES_PAGES_STATIC']
+        kw["indexes_static"] = self.config['INDEXES_STATIC']
 
         # Split in smaller lists
         lists = []
-        if kw["indexes_pages_static"]:
+        if kw["indexes_static"]:
             lists.append(posts[:kw["index_display_post_count"]])
             posts = posts[kw["index_display_post_count"]:]
             while posts:
@@ -1503,7 +1503,7 @@ class Nikola(object):
         num_pages = len(lists)
         for i, post_list in enumerate(lists):
             context = context_source.copy()
-            if kw["indexes_pages_static"]:
+            if kw["indexes_static"]:
                 ipages_i = i if i > 0 else num_pages
             else:
                 ipages_i = i + 1 if kw["indexes_pages_main"] else i
@@ -1523,7 +1523,7 @@ class Nikola(object):
             context["prevlink"] = None
             context["nextlink"] = None
             context['index_teasers'] = kw['index_teasers']
-            if kw["indexes_pages_static"]:
+            if kw["indexes_static"]:
                 if i > 0:
                     if i < num_pages - 1:
                         context["prevlink"] = page_link(i + 1, num_pages)
