e0ff482fd92eeee54cf723f6d5e40c7053319cd5
Felix Fontein <felix@fontein.de>
2014-12-21 14:39:36 +0200

    Deprecation warning for WRITE_TAG_CLOUD.

--- nikola/nikola.py
+++ nikola/nikola.py
@@ -592,6 +592,11 @@ class Nikola(object):
             utils.LOGGER.warn("DEPLOY_COMMANDS = {0}".format(self.config['DEPLOY_COMMANDS']))
             utils.LOGGER.info("(The above can be used with `nikola deploy` or `nikola deploy default`.  Multiple presets are accepted.)")
 
+        # todo: remove and change default in v8
+        if 'WRITE_TAG_CLOUD' not in config:
+            utils.LOGGER.warn("WRITE_TAG_CLOUD is not set in your config.  Defaulting to True (== writing tag_cloud_data.json).")
+            utils.LOGGER.warn("Please explicitly add the setting to your conf.py with the desired value, as the setting will default to False in the future.")
+
         # We use one global tzinfo object all over Nikola.
         self.tzinfo = dateutil.tz.gettz(self.config['TIMEZONE'])
         self.config['__tzinfo__'] = self.tzinfo
