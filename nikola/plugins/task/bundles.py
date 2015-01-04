9ae58d0620a9cdef06e6dce7663f5f83d18e2117
Chris Warrick <kwpolska@gmail.com>
2015-01-04 11:34:42 +0100

    handle bundle failures nicely
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- nikola/plugins/task/bundles.py
+++ nikola/plugins/task/bundles.py
@@ -43,11 +43,12 @@ class BuildBundles(LateTask):
     name = "create_bundles"
 
     def set_site(self, site):
-        super(BuildBundles, self).set_site(site)
+        self.logger = utils.get_logger('bundles', site.loghandlers)
         if webassets is None and self.site.config['USE_BUNDLES']:
             utils.req_missing(['webassets'], 'USE_BUNDLES', optional=True)
-            utils.LOGGER.warn('Setting USE_BUNDLES to False.')
+            self.logger.warn('Setting USE_BUNDLES to False.')
             self.site.config['USE_BUNDLES'] = False
+        super(BuildBundles, self).set_site(site)
 
     def gen_tasks(self):
         """Bundle assets using WebAssets."""
@@ -74,7 +75,12 @@ class BuildBundles(LateTask):
                 bundle = webassets.Bundle(*inputs, output=os.path.basename(output))
                 env.register(output, bundle)
                 # This generates the file
-                env[output].urls()
+                try:
+                    env[output].urls()
+                except Exception as e:
+                    self.logger.error("Failed to build bundles.")
+                    self.logger.exception(e)
+                    self.logger.notice("Try running ``nikola clean`` and building again.")
             else:
                 with open(os.path.join(out_dir, os.path.basename(output)), 'wb+'):
                     pass  # Create empty file
