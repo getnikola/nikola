300c64574fbe52f72bf85f469ebf56635e4a4f8d
Felix Fontein <felix@fontein.de>
2015-01-18 17:17:12 +0100

    Another fix.

--- nikola/post.py
+++ nikola/post.py
@@ -416,7 +416,7 @@ class Post(object):
         deps = []
         deps += self._get_dependencies(self._dependency_uptodate_page[lang])
         deps += self._get_dependencies(self._dependency_uptodate_page[None])
-        deps.add(utils.config_changed({1: sorted(self.compiler.config_dependencies)}, 'nikola.post.Post.deps_uptodate:compiler:' + self.source_path))
+        deps.append(utils.config_changed({1: sorted(self.compiler.config_dependencies)}, 'nikola.post.Post.deps_uptodate:compiler:' + self.source_path))
         return deps
 
     def compile(self, lang):
@@ -473,7 +473,7 @@ class Post(object):
         deps = []
         deps += self._get_dependencies(self._dependency_uptodate_fragment[lang])
         deps += self._get_dependencies(self._dependency_uptodate_fragment[None])
-        deps.add(utils.config_changed({1: sorted(self.compiler.config_dependencies)}, 'nikola.post.Post.deps_uptodate:compiler:' + self.source_path))
+        deps.append(utils.config_changed({1: sorted(self.compiler.config_dependencies)}, 'nikola.post.Post.deps_uptodate:compiler:' + self.source_path))
         return deps
 
     def is_translation_available(self, lang):
