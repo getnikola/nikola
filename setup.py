bea9b663cd7a7781dda236dcb591a912db3e3d1d
Chris Warrick <kwpolska@gmail.com>
2015-04-22 15:38:10 +0200

    EMERGENCY v7.3.1.1 RELEASE: doit==0.27.0
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ class nikola_install(install):
 
 
 setup(name='Nikola',
-      version='7.3.1',
+      version='7.3.1.1',
       description='A modular, fast, simple, static website generator',
       long_description=open('README.rst').read(),
       author='Roberto Alsina and others',
