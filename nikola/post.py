ce40295722c2395b04647312c665806831663540
Felix Fontein <felix@fontein.de>
2015-05-23 23:45:14 +0200

    Not changing meta, but all values for keys in meta, which are themselves dicts.

--- nikola/post.py
+++ nikola/post.py
@@ -237,8 +237,11 @@ class Post(object):
         m.update(utils.unicode_str(os.stat(self.source_path).st_mtime).encode('utf-8'))
         clean_meta = {}
         for k, v in self.meta.items():
-            if v:
-                clean_meta[k] = v
+            sub_meta = {}
+            clean_meta[k] = sub_meta
+            for kk, vv in v.items():
+                if vv:
+                    sub_meta[kk] = vv
         m.update(utils.unicode_str(json.dumps(clean_meta, cls=utils.CustomEncoder, sort_keys=True)).encode('utf-8'))
         return '<Post: {0} {1}>'.format(self.source_path, m.hexdigest())
 
