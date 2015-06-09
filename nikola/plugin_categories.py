45b593e934b1af9152424eff273a6ec4b0e0965e
Chris Warrick <kwpolska@gmail.com>
2015-06-09 18:25:08 +0200

    document `doh`
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- nikola/plugin_categories.py
+++ nikola/plugin_categories.py
@@ -279,6 +279,7 @@ class PageCompiler(BasePlugin):
         splitted = re.split('(\n\n|\r\n\r\n)', data.lstrip(), maxsplit=1)
         if len(splitted) == 1:
             return '', splitted[0]
+        # ['metadata', '\n\n', 'post content']
         return splitted[0], splitted[-1]
 
 
