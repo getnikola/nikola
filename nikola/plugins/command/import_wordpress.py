5c2d116c8334e77f278094e3f0037ae73c2c99cd
Roberto Alsina <ralsina@netmanagers.com.ar>
2015-06-12 22:00:24 -0300

    fix #1819

--- nikola/plugins/command/import_wordpress.py
+++ nikola/plugins/command/import_wordpress.py
@@ -336,9 +336,13 @@ class CommandImportWordpress(Command, ImportMixin):
         # a ton of things not supported here. We only do a basic [code
         # lang="x"] -> ```x translation, and remove quoted html entities (<,
         # >, &, and ").
-        def replacement(m):
-            language = m.group(1) or ''
-            code = m.group(2)
+        def replacement(m, c=content):
+            if len(m.groups()) == 1:
+                language = ''
+                code = m.group(0)
+            else:
+                language = m.group(1) or ''
+                code = m.group(2)
             code = code.replace('&amp;', '&')
             code = code.replace('&gt;', '>')
             code = code.replace('&lt;', '<')
