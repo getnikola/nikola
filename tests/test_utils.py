7491fe1e1d23fbe952a0456ff9f88b738f951c34
Roberto Alsina <ralsina@netmanagers.com.ar>
2015-07-17 11:51:59 -0300

    Fix #1895

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -249,7 +249,7 @@ class TranslatableSettingsTest(unittest.TestCase):
         except NameError:  # Python 3
             u = str(S)
 
-        cn = S()      #   no language specified
+        cn = S()      # no language specified
         cr = S('xx')  # real language specified
         cf = S('zz')  # fake language specified
 
