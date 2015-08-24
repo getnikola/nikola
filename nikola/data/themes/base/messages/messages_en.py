be162a0241fc653c2651fecb7b0ff03456b2bba3
Juanjo Conti <jjconti@carouselapps.com>
2015-08-24 14:34:35 -0300

    Update messages_en.py

--- nikola/data/themes/base/messages/messages_en.py
+++ nikola/data/themes/base/messages/messages_en.py
@@ -36,4 +36,6 @@ MESSAGES = {
     "Write your post here.": "Write your post here.",
     "old posts, page %d": "old posts, page %d",
     "page %d": "page %d",
+    "Authors": "Authors",
+    "Posts by %s": "Posts by %s"
 }
