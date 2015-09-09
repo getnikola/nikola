0e7626ed58a4cb753c7253cdf54ee0e0249a3026
Roberto Alsina <ralsina@netmanagers.com.ar>
2015-09-09 15:31:19 -0300

    fix #2044

--- nikola/plugins/command/auto/__init__.py
+++ nikola/plugins/command/auto/__init__.py
@@ -156,7 +156,7 @@ class CommandAuto(Command):
 
         # Do not duplicate entries -- otherwise, multiple rebuilds are triggered
         watched = set([
-            'templates/',
+            'templates/', 'plugins/',
         ] + [get_theme_path(name) for name in self.site.THEMES])
         for item in self.site.config['post_pages']:
             watched.add(os.path.dirname(item[0]))
