76b843af23613eef5899cdaa0118c36837837af4
Manuel Kaufmann <humitos@gmail.com>
2016-01-04 19:43:06 -0500

    Better configs for gallery's thumbnails
    * Do not use a fixed number of columns
    * Translatable index title
    * False in GALLERIES_USE_THUMBNAIL to make it backward compatible
    * Use filter "h" in Mako templates for display titles

--- nikola/plugins/task/galleries.py
+++ nikola/plugins/task/galleries.py
@@ -91,7 +91,6 @@ class Galleries(Task, ImageProcessor):
             'generate_rss': site.config['GENERATE_RSS'],
             'gallery_index_title': site.config['GALLERY_INDEX_TITLE'],
             'galleries_use_thumbnail': site.config['GALLERIES_USE_THUMBNAIL'],
-            'galleries_columns': site.config['GALLERIES_COLUMNS'],
             'galleries_default_thumbnail': site.config['GALLERIES_DEFAULT_THUMBNAIL'],
         }
 
@@ -238,7 +237,7 @@ class Galleries(Task, ImageProcessor):
                     context["title"] = post.title(lang)
                 else:
                     # TODO: make this translatable
-                    context["title"] = self.kw['gallery_index_title']
+                    context["title"] = self.kw['gallery_index_title'](lang)
 
                 context["description"] = None
 
@@ -292,7 +291,6 @@ class Galleries(Task, ImageProcessor):
                 context["thumbnail_size"] = self.kw["thumbnail_size"]
                 context["pagekind"] = ["gallery_front"]
                 context["galleries_use_thumbnail"] = self.kw["galleries_use_thumbnail"]
-                context["galleries_columns"] = self.kw["galleries_columns"]
                 context["galleries_default_thumbnail"] = self.kw["galleries_default_thumbnail"]
 
                 if post:
