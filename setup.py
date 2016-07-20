e0b66710e38fea539b1aabf33026f833fa0b7aff
Roberto Alsina <ralsina@netmanagers.com.ar>
2016-07-20 12:51:37 -0300

    v7.7.10

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ class nikola_install(install):
 
 
 setup(name='Nikola',
-      version='7.7.9',
+      version='7.7.10',
       description='A modular, fast, simple, static website and blog generator',
       long_description=open('README.rst').read(),
       author='Roberto Alsina and others',
