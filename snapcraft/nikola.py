095d5ff1da784f554ca9f112c5335397030f585b
Roberto Alsina <ralsina@netmanagers.com.ar>
2016-07-22 13:29:13 -0300

    Smaller build, no more notebook (fails to package)

---
+++ snapcraft/nikola.py
@@ -0,0 +1,10 @@
+#!/snap/nikola/current/usr/bin/python3
+# EASY-INSTALL-ENTRY-SCRIPT: 'Nikola==7.7.11'
+__requires__ = 'Nikola==7.7.11'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('Nikola==7.7.11', 'console_scripts', 'nikola')()
+    )
