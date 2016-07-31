d28f0f8e95e1263b7d3bb4539bec72580531c3da
Timo Kankare <timo.kankare@iki.fi>
2016-07-31 22:49:20 +0300

    Modified resize_image to use transpose instead of rotate. Transpose changes the image size correctly.

--- nikola/image_processing.py
+++ nikola/image_processing.py
@@ -117,11 +117,11 @@ class ImageProcessor(object):
             # Rotate according to EXIF
             value = exif['0th'].get(piexif.ImageIFD.Orientation, 1)
             if value in (3, 4):
-                im = im.rotate(180)
+                im = im.transpose(Image.ROTATE_180)
             elif value in (5, 6):
-                im = im.rotate(270)
+                im = im.transpose(Image.ROTATE_270)
             elif value in (7, 8):
-                im = im.rotate(90)
+                im = im.transpose(Image.ROTATE_90)
             if value in (2, 4, 5, 7):
                 im = ImageOps.mirror(im)
             exif['0th'][piexif.ImageIFD.Orientation] = 1
