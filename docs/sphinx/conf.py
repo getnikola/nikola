e8b206421d2258ba92f9d8f24063d7a1cd07d932
Chris Warrick <kwpolska@gmail.com>
2016-08-04 20:19:52 +0200

    Remove incompatible Sphinx plugin
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -28,11 +28,7 @@ from __future__ import unicode_literals
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax']
-try:
-    import sphinxcontrib.gist  # NOQA
-    extensions.append('sphinxcontrib.gist')
-except ImportError:
-    pass
+# extensions.append('sphinxcontrib.gist')
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
