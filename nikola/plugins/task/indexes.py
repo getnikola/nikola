8a9441bbac4dbcc58debab44199e655f12ca97ec
Roberto Alsina <ralsina@netmanagers.com.ar>
2016-08-19 23:05:42 -0300

    fix path conflict

--- nikola/plugins/task/indexes.py
+++ nikola/plugins/task/indexes.py
@@ -184,8 +184,8 @@ class Indexes(Task):
                         deps += post.deps(lang)
                         deps_uptodate += post.deps_uptodate(lang)
 
-                    feed_url = urljoin(self.site.config['BASE_URL'], self.site.link('section_index_rss', None, lang).lstrip('/'))
-                    output_name = os.path.join(kw['output_folder'], section_slug, 'rss.xml')
+                    feed_url = urljoin(self.site.config['BASE_URL'], self.site.link('section_index_rss', section_slug, lang).lstrip('/'))
+                    output_name = os.path.join(kw['output_folder'], self.site.link('section_index_rss', section_slug, lang).lstrip(os.sep))
                     task = {
                         'basename': self.name,
                         'name': os.path.normpath(output_name),
