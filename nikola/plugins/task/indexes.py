681b0e72f16cd3c21a4ef41beb38f3028017ffbd
Felix Fontein <felix@fontein.de>
2016-10-17 23:52:17 +0200

    Merging rss and leftovers of indexes plugin into taxonomy plugin.

--- nikola/plugins/task/indexes.py
+++ nikola/plugins/task/indexes.py
@@ -27,117 +27,60 @@
 """Render the blog indexes."""
 
 from __future__ import unicode_literals
-import os
 
-from nikola.plugin_categories import Task
-from nikola import utils
+from nikola.plugin_categories import Taxonomy
 
 
-class Indexes(Task):
-    """Render the blog indexes."""
+class Indexes(Taxonomy):
+    """Classify for the blog's main index."""
 
-    name = "render_indexes"
+    name = "classify_indexes"
+
+    classification_name = "index"
+    metadata_name = None
+    overview_page_variable_name = None
+    more_than_one_classifications_per_post = False
+    has_hierarchy = False
+    show_list_as_index = True
+    template_for_list_of_one_classification = "index.tmpl"
+    template_for_classification_overview = None
+    apply_to_posts = True
+    apply_to_pages = False
+    omit_empty_classifications = False
+    also_create_classifications_from_other_languages = False
 
     def set_site(self, site):
         """Set Nikola site."""
-        self.number_of_pages = dict()
-        site.register_path_handler('index', self.index_path)
-        site.register_path_handler('index_atom', self.index_atom_path)
+        # Redirect automatically generated 'index_rss' path handler to 'rss' for compatibility with old rss plugin
+        site.register_path_handler('rss', lambda name, lang: site.path_handlers['index_rss'](name, lang))
         return super(Indexes, self).set_site(site)
 
-    def _get_filtered_posts(self, lang, show_untranslated_posts):
-        """Return a filtered list of all posts for the given language.
+    def classify(self, post, lang):
+        """Classify the given post for the given language."""
+        return ""
 
-        If show_untranslated_posts is True, will only include posts which
-        are translated to the given language. Otherwise, returns all posts.
-        """
-        if show_untranslated_posts:
-            return self.site.posts
-        else:
-            return [x for x in self.site.posts if x.is_translation_available(lang)]
+    def get_path(self, classification, lang, type='page'):
+        """A path handler for the given classification."""
+        if type == 'rss':
+            return [self.site.config['RSS_PATH']], True
+        # 'page' (index) or 'feed' (Atom)
+        page_number = None
+        if type == 'page':
+            # Interpret argument as page number
+            try:
+                page_number = int(classification)
+            except:
+                pass
+        return [self.site.config['INDEX_PATH']], True, page_number
 
-    def _compute_number_of_pages(self, filtered_posts, posts_count):
-        """Given a list of posts and the maximal number of posts per page, computes the number of pages needed."""
-        return min(1, (len(filtered_posts) + posts_count - 1) // posts_count)
-
-    def gen_tasks(self):
-        """Render the blog indexes."""
-        self.site.scan_posts()
-        yield self.group_task()
-
+    def provide_context_and_uptodate(self, classification, lang):
+        """Provide data for the context and the uptodate list for the list of the given classifiation."""
         kw = {
-            "translations": self.site.config['TRANSLATIONS'],
-            "messages": self.site.MESSAGES,
-            "output_folder": self.site.config['OUTPUT_FOLDER'],
-            "feed_length": self.site.config['FEED_LENGTH'],
-            "feed_links_append_query": self.site.config["FEED_LINKS_APPEND_QUERY"],
-            "feed_teasers": self.site.config["FEED_TEASERS"],
-            "feed_plain": self.site.config["FEED_PLAIN"],
-            "filters": self.site.config['FILTERS'],
-            "index_file": self.site.config['INDEX_FILE'],
-            "show_untranslated_posts": self.site.config['SHOW_UNTRANSLATED_POSTS'],
-            "index_display_post_count": self.site.config['INDEX_DISPLAY_POST_COUNT'],
-            "indexes_title": self.site.config['INDEXES_TITLE'],
-            "strip_indexes": self.site.config['STRIP_INDEXES'],
-            "blog_title": self.site.config["BLOG_TITLE"],
-            "generate_atom": self.site.config["GENERATE_ATOM"],
-            "site_url": self.site.config["SITE_URL"],
         }
-
-        template_name = "index.tmpl"
-        for lang in kw["translations"]:
-            def page_link(i, displayed_i, num_pages, force_addition, extension=None):
-                feed = "_atom" if extension == ".atom" else ""
-                return utils.adjust_name_for_index_link(self.site.link("index" + feed, None, lang), i, displayed_i,
-                                                        lang, self.site, force_addition, extension)
-
-            def page_path(i, displayed_i, num_pages, force_addition, extension=None):
-                feed = "_atom" if extension == ".atom" else ""
-                return utils.adjust_name_for_index_path(self.site.path("index" + feed, None, lang), i, displayed_i,
-                                                        lang, self.site, force_addition, extension)
-
-            filtered_posts = self._get_filtered_posts(lang, kw["show_untranslated_posts"])
-
-            indexes_title = kw['indexes_title'](lang) or kw['blog_title'](lang)
-            self.number_of_pages[lang] = self._compute_number_of_pages(filtered_posts, kw['index_display_post_count'])
-
-            context = {}
-            context["pagekind"] = ["main_index", "index"]
-
-            yield self.site.generic_index_renderer(lang, filtered_posts, indexes_title, template_name, context, kw, 'render_indexes', page_link, page_path)
-
-    def index_path(self, name, lang, is_feed=False):
-        """Link to a numbered index.
-
-        Example:
-
-        link://index/3 => /index-3.html
-        """
-        extension = None
-        if is_feed:
-            extension = ".atom"
-            index_file = os.path.splitext(self.site.config['INDEX_FILE'])[0] + extension
-        else:
-            index_file = self.site.config['INDEX_FILE']
-        if lang in self.number_of_pages:
-            number_of_pages = self.number_of_pages[lang]
-        else:
-            number_of_pages = self._compute_number_of_pages(self._get_filtered_posts(lang, self.site.config['SHOW_UNTRANSLATED_POSTS']), self.site.config['INDEX_DISPLAY_POST_COUNT'])
-            self.number_of_pages[lang] = number_of_pages
-        return utils.adjust_name_for_index_path_list([_f for _f in [self.site.config['TRANSLATIONS'][lang],
-                                                                    self.site.config['INDEX_PATH'],
-                                                                    index_file] if _f],
-                                                     name,
-                                                     utils.get_displayed_page_number(name, number_of_pages, self.site),
-                                                     lang,
-                                                     self.site,
-                                                     extension=extension)
-
-    def index_atom_path(self, name, lang):
-        """Link to a numbered Atom index.
-
-        Example:
-
-        link://index_atom/3 => /index-3.atom
-        """
-        return self.index_path(name, lang, is_feed=True)
+        context = {
+            "title": self.site.config["BLOG_TITLE"](lang),
+            "classification_title": "",
+            "description": self.site.config["BLOG_DESCRIPTION"](lang),
+        }
+        kw.update(context)
+        return context, kw
