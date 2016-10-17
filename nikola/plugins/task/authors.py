78167512c0b287e211a625cf872c3dcd3c072260
Felix Fontein <felix@fontein.de>
2016-10-17 08:00:14 +0200

    Considering only relevant posts.

--- nikola/plugins/task/authors.py
+++ nikola/plugins/task/authors.py
@@ -109,7 +109,13 @@ class ClassifyAuthors(Taxonomy):
         """Rearrange, modify or otherwise use the list of posts per classification and per language."""
         more_than_one = False
         for lang, posts_per_author in posts_per_author_per_language.items():
-            if len(posts_per_author.keys()) > 1:
+            authors = set()
+            for author, posts in posts_per_author.items():
+                for post in posts:
+                    if not self.site.config["SHOW_UNTRANSLATED_POSTS"] and not post.is_translation_available(lang):
+                        continue
+                    authors.add(author)
+            if len(authors) > 1:
                 more_than_one = True
         self.generate_author_pages = self.site.config["ENABLE_AUTHOR_PAGES"] and more_than_one
         self.site.GLOBAL_CONTEXT["author_pages_generated"] = self.generate_author_pages
--- nikola/plugins/task/sections.py
+++ nikola/plugins/task/sections.py
@@ -109,5 +109,11 @@ class ClassifySections(Taxonomy):
     def postprocess_posts_per_classification(self, posts_per_section_per_language, flat_hierarchy_per_lang=None, hierarchy_lookup_per_lang=None):
         """Rearrange, modify or otherwise use the list of posts per classification and per language."""
         for lang, posts_per_section in posts_per_section_per_language.items():
-            # don't build sections when there is only one, aka. default setups
-            self.enable_for_lang[lang] = (len(posts_per_section.keys()) > 1)
+            # Don't build sections when there is only one, a.k.a. default setups
+            sections = set()
+            for section, posts in posts_per_section.items():
+                for post in posts:
+                    if not self.site.config["SHOW_UNTRANSLATED_POSTS"] and not post.is_translation_available(lang):
+                        continue
+                    sections.add(section)
+            self.enable_for_lang[lang] = (len(sections) > 1)
