4b08acb8f7246838cd345beddd7ddfc5c5463ef1
Roberto Alsina <ralsina@netmanagers.com.ar>
2016-10-19 17:49:10 +0200

    Yes, I am at a snapcraft sprint, so I will get this in shape

--- snapcraft/stable/build.sh
+++ snapcraft/stable/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-snapcraft
-cp ../nikola.py prime/usr/bin/nikola
+snapcraft pull
+pip3 wheel --wheel-dir parts/nikola/packages --disable-pip-version-check --no-index --find-links parts/nikola/packages pillow
+snapcraft build
+snapcraft stage
 find prime/ -name '*.a' -exec rm {} \;
-snapcraft
+snapcraft snap
 
