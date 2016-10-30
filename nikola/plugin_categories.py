71eb72f752d871209e4e0845f82d13aa9df75eb7
Felix Fontein <felix@fontein.de>
2016-10-30 11:53:15 +0100

    Put function stub in wrong class.

--- nikola/plugin_categories.py
+++ nikola/plugin_categories.py
@@ -94,6 +94,10 @@ class PostScanner(BasePlugin):
         """Create a list of posts from some source. Returns a list of Post objects."""
         raise NotImplementedError()
 
+    def get_appearing_post_extensions(self):
+        """Returns a list of post extensions which appear, or None if such a list cannot be computed efficiently."""
+        return None
+
 
 class Command(BasePlugin, DoitCommand):
     """Doit command implementation."""
@@ -135,10 +139,6 @@ class Command(BasePlugin, DoitCommand):
         @param args (list) list of positional arguments
         """
         raise NotImplementedError()
-
-    def get_appearing_post_extensions(self):
-        """Returns a list of post extensions which appear, or None if such a list cannot be computed efficiently."""
-        return None
 
 
 def help(self):
