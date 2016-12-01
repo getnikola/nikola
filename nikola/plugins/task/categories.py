38b30ce6bec00353bee006e8a5e764ba6ac42c17
Felix Fontein <felix@fontein.de>
2016-12-01 23:48:54 +0100

    Making pydocstyle happy.

--- nikola/plugins/task/categories.py
+++ nikola/plugins/task/categories.py
@@ -62,6 +62,7 @@ class ClassifyCategories(Taxonomy):
     also_create_classifications_from_other_languages = True
 
     def set_site(self, site):
+        """Set site, which is a Nikola instance."""
         super(ClassifyCategories, self).set_site(site)
         self.show_list_as_index = self.site.config['CATEGORY_PAGES_ARE_INDEXES']
         self.template_for_single_list = "tagindex.tmpl" if self.show_list_as_index else "tag.tmpl"
--- nikola/plugins/task/tags.py
+++ nikola/plugins/task/tags.py
@@ -58,6 +58,7 @@ class ClassifyTags(Taxonomy):
     also_create_classifications_from_other_languages = True
 
     def set_site(self, site):
+        """Set site, which is a Nikola instance."""
         super(ClassifyTags, self).set_site(site)
         self.show_list_as_index = self.site.config['TAG_PAGES_ARE_INDEXES']
         self.template_for_single_list = "tagindex.tmpl" if self.show_list_as_index else "tag.tmpl"
