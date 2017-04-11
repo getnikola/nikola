05aee659b2ea11989dfa294218271eb07c0bb032
Chris Warrick <kwpolska@gmail.com>
2017-04-11 19:44:36 +0200

    Use official ipykernel/notebok wheels

--- scripts/getwheelhouse.sh
+++ scripts/getwheelhouse.sh
@@ -2,6 +2,6 @@
 for i in $@; do
     wget https://github.com/getnikola/wheelhouse/archive/v$i'.zip'
     unzip 'v'$i'.zip'
-    pip install --use-wheel --no-index --find-links=wheelhouse-$i lxml Pillow ipykernel notebook PyYAML
+    pip install --use-wheel --no-index --find-links=wheelhouse-$i lxml Pillow PyYAML
     rm -rf wheelhouse-$i 'v'$i'.zip'
 done
