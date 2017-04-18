80ce165aa8a9fc280490d2c8dcb95eb27f27ece1
Felix Fontein <felix@fontein.de>
2017-04-18 22:16:30 +0200

    Fixes check which should avoid name clashes.

--- nikola/plugins/task/sections.py
+++ nikola/plugins/task/sections.py
@@ -142,9 +142,11 @@ link://section_index_rss/cars => /cars/rss.xml""",
     def should_generate_classification_page(self, dirname, post_list, lang):
         """Only generates list of posts for classification if this function returns True."""
         short_destination = dirname + '/' + self.site.config['INDEX_FILE']
-        for post in post_list:
-            # If there is an index.html pending to be created from a page, do not generate the section page.
-            # The section page would be useless anyways. (via Issue #2613)
+        # If there is an index.html pending to be created from a page, do not generate the section page.
+        # The section page would be useless anyways. (via Issue #2613)
+        for post in self.site.timeline:
+            if not self.site.config["SHOW_UNTRANSLATED_POSTS"] and not post.is_translation_available(lang):
+                continue
             if post.destination_path(lang, sep='/') == short_destination:
                 return False
         return True
