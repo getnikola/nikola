0f06b98493f40eac082093b525c875ad03361cfa
Roberto Alsina <ralsina@netmanagers.com.ar>
2017-05-17 09:43:08 -0300

    Fix #2740

--- nikola/plugins/command/auto/__init__.py
+++ nikola/plugins/command/auto/__init__.py
@@ -270,7 +270,7 @@ class CommandAuto(Command):
         if (fname.endswith('~') or
                 fname.startswith('.') or
                 '__pycache__' in event_path or
-                event_path.endswith(('.pyc', '.pyo', '.pyd')) or
+                event_path.endswith(('.pyc', '.pyo', '.pyd', '_bak')) or
                 os.path.isdir(event_path)):  # Skip on folders, these are usually duplicates
             return
         self.logger.info('REBUILDING SITE (from {0})'.format(event_path))
