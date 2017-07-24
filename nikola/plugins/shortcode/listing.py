0f0adb38b9e9bb17212c3da46aafaffe4a68e8c3
Roberto Alsina <ralsina@onapsis.com>
2017-07-24 16:44:21 -0300

    line numbers support

--- nikola/plugins/shortcode/listing.py
+++ nikola/plugins/shortcode/listing.py
@@ -24,9 +24,8 @@ class Plugin(ShortcodePlugin):
         Plugin.folders = site.config['LISTINGS_FOLDERS']
         return super(Plugin, self).set_site(site)
 
-    def handler(self, fname, language='text', filename=None, site=None, data=None, lang=None, post=None):
+    def handler(self, fname, language='text', linenumbers=False, filename=None, site=None, data=None, lang=None, post=None):
         """Create HTML for a listing."""
-
         fname = fname.replace('/', os.sep)
         if len(self.folders) == 1:
             listings_folder = next(iter(self.folders.keys()))
@@ -36,11 +35,12 @@ class Plugin(ShortcodePlugin):
                 fpath = os.path.join(listings_folder, fname)  # old syntax: don't specify folder name
         else:
             fpath = os.path.join(fname)  # must be new syntax: specify folder name
-
+        linenumbers = 'table' if linenumbers else False
         deps = []
         with open(fpath, 'r') as inf:
             data = inf.read()
             lexer = pygments.lexers.get_lexer_by_name(language)
-            formatter = pygments.formatters.get_formatter_by_name('html')
+            formatter = pygments.formatters.get_formatter_by_name('html', linenos=linenumbers)
             output = pygments.highlight(data, lexer, formatter)
+
         return output, deps
