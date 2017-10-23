cee31c2f49a7b6a295b11bc5967b39b07fdaf4a7
Chris Warrick <kwpolska@gmail.com>
2017-10-23 17:30:50 +0200

    Use nikola default_config in scripts/release
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- scripts/generate_conf.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""Generate a conf.py file from the template, using default settings."""
-
-import nikola.plugins.command.init
-
-try:
-    print(nikola.plugins.command.init.CommandInit.create_configuration_to_string())
-except:
-    print(nikola.plugins.command.init.CommandInit.create_configuration_to_string().encode('utf-8'))
