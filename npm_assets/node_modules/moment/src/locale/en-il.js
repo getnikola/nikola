b7e8b6c78a582f1740f3f3ac4341e51795ffd086
Chris Warrick <kwpolska@gmail.com>
2018-03-18 15:21:33 +0100

    Update npm assets
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- npm_assets/node_modules/moment/src/locale/en-il.js
+++ npm_assets/node_modules/moment/src/locale/en-il.js
@@ -1,26 +1,22 @@
 //! moment.js locale configuration
+//! locale : English (Israel) [en-il]
+//! author : Chris Gedrim : https://github.com/chrisgedrim
 
-;(function (global, factory) {
-   typeof exports === 'object' && typeof module !== 'undefined'
-       && typeof require === 'function' ? factory(require('../moment')) :
-   typeof define === 'function' && define.amd ? define(['../moment'], factory) :
-   factory(global.moment)
-}(this, (function (moment) { 'use strict';
+import moment from '../moment';
 
-
-var enAu = moment.defineLocale('en-au', {
+export default moment.defineLocale('en-il', {
     months : 'January_February_March_April_May_June_July_August_September_October_November_December'.split('_'),
     monthsShort : 'Jan_Feb_Mar_Apr_May_Jun_Jul_Aug_Sep_Oct_Nov_Dec'.split('_'),
     weekdays : 'Sunday_Monday_Tuesday_Wednesday_Thursday_Friday_Saturday'.split('_'),
     weekdaysShort : 'Sun_Mon_Tue_Wed_Thu_Fri_Sat'.split('_'),
     weekdaysMin : 'Su_Mo_Tu_We_Th_Fr_Sa'.split('_'),
     longDateFormat : {
-        LT : 'h:mm A',
-        LTS : 'h:mm:ss A',
+        LT : 'HH:mm',
+        LTS : 'HH:mm:ss',
         L : 'DD/MM/YYYY',
         LL : 'D MMMM YYYY',
-        LLL : 'D MMMM YYYY h:mm A',
-        LLLL : 'dddd, D MMMM YYYY h:mm A'
+        LLL : 'D MMMM YYYY HH:mm',
+        LLLL : 'dddd, D MMMM YYYY HH:mm'
     },
     calendar : {
         sameDay : '[Today at] LT',
@@ -34,7 +30,6 @@ var enAu = moment.defineLocale('en-au', {
         future : 'in %s',
         past : '%s ago',
         s : 'a few seconds',
-        ss : '%d seconds',
         m : 'a minute',
         mm : '%d minutes',
         h : 'an hour',
@@ -54,13 +49,6 @@ var enAu = moment.defineLocale('en-au', {
             (b === 2) ? 'nd' :
             (b === 3) ? 'rd' : 'th';
         return number + output;
-    },
-    week : {
-        dow : 1, // Monday is the first day of the week.
-        doy : 4  // The week that contains Jan 4th is the first week of the year.
     }
 });
 
-return enAu;
-
-})));
--- npm_assets/node_modules/popper.js/index.d.ts
+++ npm_assets/node_modules/popper.js/index.d.ts
@@ -1,10 +1,15 @@
-// Type definitions for popper.js 1.10
-// Project: https://github.com/FezVrasta/popper.js/
-// Definitions by: edcarroll <https://github.com/edcarroll>, ggray <https://github.com/giladgray>, rhysd <https://rhysd.github.io>, joscha <https://github.com/joscha>, seckardt <https://github.com/seckardt>, marcfallows <https://github.com/marcfallows>
+/**
+ * @fileoverview This file only declares the public portions of the API.
+ * It should not define internal pieces such as utils or modifier details.
+ *
+ * Original definitions by: edcarroll <https://github.com/edcarroll>, ggray <https://github.com/giladgray>, rhysd <https://rhysd.github.io>, joscha <https://github.com/joscha>, seckardt <https://github.com/seckardt>, marcfallows <https://github.com/marcfallows>
+ */
 
-// This file only declares the public portions of the API.
-// It should not define internal pieces such as utils or modifier details.
-
+/**
+ * This kind of namespace declaration is not necessary, but is kept here for backwards-compatibility with
+ * popper.js 1.x. It can be removed in 2.x so that the default export is simply the Popper class
+ * and all the types / interfaces are top-level named exports.
+ */
 declare namespace Popper {
   export type Position = 'top' | 'right' | 'bottom' | 'left';
 
@@ -25,6 +30,8 @@ declare namespace Popper {
     | 'left-start';
 
   export type Boundary = 'scrollParent' | 'viewport' | 'window';
+
+  export type Behavior = 'flip' | 'clockwise' | 'counterclockwise';
 
   export type ModifierFn = (data: Data, options: Object) => Data;
 
@@ -50,7 +57,7 @@ declare namespace Popper {
       element?: string | Element,
     };
     flip?: BaseModifier & {
-      behavior?: 'flip' | 'clockwise' | 'counterclockwise' | Position[],
+      behavior?: Behavior | Position[],
       padding?: number,
       boundariesElement?: Boundary | Element,
     };
@@ -61,10 +68,11 @@ declare namespace Popper {
       gpuAcceleration?: boolean,
     };
     computeStyle?: BaseModifier & {
-        gpuAcceleration?: boolean;
-        x?: 'bottom' | 'top',
-        y?: 'left' | 'right'
+      gpuAcceleration?: boolean;
+      x?: 'bottom' | 'top',
+      y?: 'left' | 'right'
     };
+
     [name: string]: (BaseModifier & Record<string, any>) | undefined;
   }
 
@@ -96,36 +104,56 @@ declare namespace Popper {
 
   export interface PopperOptions {
     placement?: Placement;
+    positionFixed?: boolean;
     eventsEnabled?: boolean;
     modifiers?: Modifiers;
     removeOnDestroy?: boolean;
+
     onCreate?(data: Data): void;
+
     onUpdate?(data: Data): void;
   }
 
   export interface ReferenceObject {
     clientHeight: number;
     clientWidth: number;
+
     getBoundingClientRect(): ClientRect;
   }
 }
 
+// Re-export types in the Popper namespace so that they can be accessed as top-level named exports.
+// These re-exports should be removed in 2.x when the "declare namespace Popper" syntax is removed.
+export type Position = Popper.Position;
+export type Placement = Popper.Placement;
+export type Boundary = Popper.Boundary;
+export type Behavior = Popper.Behavior;
+export type ModifierFn = Popper.ModifierFn;
+export type BaseModifier = Popper.BaseModifier;
+export type Modifiers = Popper.Modifiers;
+export type Offset = Popper.Offset;
+export type Data = Popper.Data;
+export type PopperOptions = Popper.PopperOptions;
+export type ReferenceObject = Popper.ReferenceObject;
+
 declare class Popper {
-  static modifiers: (Popper.BaseModifier & { name: string })[];
-  static placements: Popper.Placement[];
-  static Defaults: Popper.PopperOptions;
+  static modifiers: (BaseModifier & { name: string })[];
+  static placements: Placement[];
+  static Defaults: PopperOptions;
 
-  options: Popper.PopperOptions;
+  options: PopperOptions;
 
-  constructor(reference: Element | Popper.ReferenceObject, popper: Element, options?: Popper.PopperOptions);
+  constructor(reference: Element | ReferenceObject, popper: Element, options?: PopperOptions);
 
   destroy(): void;
+
   update(): void;
+
   scheduleUpdate(): void;
+
   enableEventListeners(): void;
+
   disableEventListeners(): void;
 }
 
-declare module 'popper.js' {
-  export default Popper;
-}
+export default Popper;
