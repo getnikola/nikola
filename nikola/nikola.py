987f18610fb76545d0ca4d705e6f6dd976445949
Chris Warrick <kwpolska@gmail.com>
2018-04-15 14:45:27 +0200

    Fix #3040 -- fix RSS_PATH/translatable setting rebuild issues
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- nikola/nikola.py
+++ nikola/nikola.py
@@ -678,6 +678,7 @@ class Nikola(object):
                                              'posts_section_name',
                                              'posts_section_title',
                                              'front_index_header',
+                                             'rss_path',
                                              )
         # WARNING: navigation_links SHOULD NOT be added to the list above.
         #          Themes ask for [lang] there and we should provide it.
