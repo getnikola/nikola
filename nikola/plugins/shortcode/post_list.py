f7ec3d65c6b04296248030dcba671624b3fd8df2
Roberto Alsina <ralsina@netmanagers.com.ar>
2018-04-15 13:02:32 -0300

    docstrings

--- nikola/plugins/shortcode/post_list.py
+++ nikola/plugins/shortcode/post_list.py
@@ -24,7 +24,7 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-"""Post list directive for reStructuredText."""
+"""Post list shortcut."""
 
 
 import operator
