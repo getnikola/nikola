e5a9e648a01f95e7398f658af7e2e6afe8ac3435
Chris Warrick <kwpolska@gmail.com>
2018-04-16 09:48:01 +0200

    shortcut → shortcode
    [ci skip]

--- nikola/plugins/shortcode/post_list.py
+++ nikola/plugins/shortcode/post_list.py
@@ -24,7 +24,7 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-"""Post list shortcut."""
+"""Post list shortcode."""
 
 
 import operator
