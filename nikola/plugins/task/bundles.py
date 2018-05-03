e22799a5af2cbf037fa82958ac598828f5331cff
Chris Warrick <kwpolska@gmail.com>
2018-05-03 18:22:23 +0200

    Hide webassets warning if the site is not configured
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- nikola/plugins/task/bundles.py
+++ nikola/plugins/task/bundles.py
@@ -46,7 +46,7 @@ class BuildBundles(LateTask):
     def set_site(self, site):
         """Set Nikola site."""
         super(BuildBundles, self).set_site(site)
-        if webassets is None and site.config['USE_BUNDLES']:
+        if webassets is None and site.configured and site.config['USE_BUNDLES']:
             utils.req_missing(['webassets'], 'USE_BUNDLES', optional=True)
             self.logger.warn('Setting USE_BUNDLES to False.')
             site.config['USE_BUNDLES'] = False
