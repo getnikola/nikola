dd8d26956ddf37403d1b350db5eb92669d101dcc
Manuel Thalmann <m@nuth.ch>
2019-02-27 02:28:27 +0100

    Add tests for the new conf-file handling

---
+++ tests/test_config.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Script to test whether config-files are loaded correctly.
+"""
+import sys
+import os
+import random
+import pytest
+import unittest
+from nikola import __main__ as nikola
+from .base import BaseTestCase
+
+class ConfigTest(BaseTestCase):
+    def setUp(self):
+        script_root = os.path.dirname(__file__)
+        test_dir = os.path.join(script_root, "data", "test_config")
+        nikola.main(["--conf=" + os.path.join(test_dir, "conf.py")])
+        self.simple_config = nikola.config
+        options = list(self.simple_config.keys())
+        options.remove("ADDITIONAL_METADATA")
+        self.option = random.choice(options)
+        nikola.main(["--conf=" + os.path.join(test_dir, "prod.py")])
+        self.complex_config = nikola.config
+        pass
+
+    def test_simple_config(self):
+        assert self.simple_config["ADDITIONAL_METADATA"]["ID"] == "conf"
+
+    def test_inherited_config(self):
+        assert self.simple_config[self.option] == self.complex_config[self.option]
+        assert self.complex_config["ADDITIONAL_METADATA"]["ID"] == "prod"
+
+if __name__ == '__main__':
+    unittest.main()
