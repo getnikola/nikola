a17c513da4b957c681ba74caa7b0aeba06d75114
Manuel Thalmann <m@nuth.ch>
2019-02-28 21:16:04 +0100

    Add a unit-test against filename-restrictions

--- tests/test_config.py
+++ tests/test_config.py
@@ -11,13 +11,16 @@ from .base import BaseTestCase
 
 class ConfigTest(BaseTestCase):
     """Provides tests for the configuration-file handling."""
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         script_root = os.path.dirname(__file__)
         test_dir = os.path.join(script_root, "data", "test_config")
         nikola.main(["--conf=" + os.path.join(test_dir, "conf.py")])
         self.simple_config = nikola.config
         nikola.main(["--conf=" + os.path.join(test_dir, "prod.py")])
         self.complex_config = nikola.config
+        nikola.main(["--conf=" + os.path.join(test_dir, "config.with+illegal(module)name.characters.py")])
+        self.complex_filename_config = nikola.config
 
         options = list()
         for option in self.simple_config.keys():
@@ -33,6 +36,11 @@ class ConfigTest(BaseTestCase):
         """Checks whether configuration-files with ineritance areinterpreted correctly."""
         assert self.simple_config[self.option] == self.complex_config[self.option]
         assert self.complex_config["ADDITIONAL_METADATA"]["ID"] == "prod"
+
+    def test_config_with_illegal_filename(self):
+        """Checks whether files with illegal module-name characters can be set as config-files, too."""
+        assert self.simple_config[self.option] == self.complex_filename_config[self.option]
+        assert self.complex_filename_config["ADDITIONAL_METADATA"]["ID"] == "illegal"
 
 
 if __name__ == '__main__':
