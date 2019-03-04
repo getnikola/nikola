b759a804ad85b07d1763c608c426d874fbca0a12
Manuel Thalmann <m@nuth.ch>
2019-03-04 19:18:38 +0100

    Rework the way to check option-inheritance

--- tests/test_config.py
+++ tests/test_config.py
@@ -13,6 +13,7 @@ class ConfigTest(BaseTestCase):
     """Provides tests for the configuration-file handling."""
     @classmethod
     def setUpClass(self):
+        self.metadata_option = "ADDITIONAL_METADATA"
         script_root = os.path.dirname(__file__)
         test_dir = os.path.join(script_root, "data", "test_config")
         nikola.main(["--conf=" + os.path.join(test_dir, "conf.py")])
@@ -21,26 +22,28 @@ class ConfigTest(BaseTestCase):
         self.complex_config = nikola.config
         nikola.main(["--conf=" + os.path.join(test_dir, "config.with+illegal(module)name.characters.py")])
         self.complex_filename_config = nikola.config
+        self.check_base_equality(self.complex_filename_config)
 
-        options = list()
+    @classmethod
+    def check_base_equality(self, config):
+        """Checks whether the specified `config` equals the base config."""
         for option in self.simple_config.keys():
-            if re.match("^[A-Z]+(_[A-Z]+)*$", option) and option != "ADDITIONAL_METADATA":
-                options.append(option)
-        self.option = random.choice(options)
+            if re.match("^[A-Z]+(_[A-Z]+)*$", option) and option != self.metadata_option:
+                assert self.simple_config[option] == self.complex_config[option]
 
     def test_simple_config(self):
         """Checks whether configuration-files without ineritance are interpreted correctly."""
-        assert self.simple_config["ADDITIONAL_METADATA"]["ID"] == "conf"
+        assert self.simple_config[self.metadata_option]["ID"] == "conf"
 
     def test_inherited_config(self):
         """Checks whether configuration-files with ineritance are interpreted correctly."""
-        assert self.simple_config[self.option] == self.complex_config[self.option]
-        assert self.complex_config["ADDITIONAL_METADATA"]["ID"] == "prod"
+        self.check_base_equality(config=self.complex_config)
+        assert self.complex_config[self.metadata_option]["ID"] == "prod"
 
     def test_config_with_illegal_filename(self):
         """Checks whether files with illegal module-name characters can be set as config-files, too."""
-        assert self.simple_config[self.option] == self.complex_filename_config[self.option]
-        assert self.complex_filename_config["ADDITIONAL_METADATA"]["ID"] == "illegal"
+        self.check_base_equality(config=self.complex_filename_config)
+        assert self.complex_filename_config[self.metadata_option]["ID"] == "illegal"
 
 
 if __name__ == '__main__':
