87eda5e1e29043231749a81566552631b50df616
Roberto Alsina <ralsina@netmanagers.com.ar>
2019-08-07 10:45:20 -0300

    Updated NPM dependencies

--- npm_assets/node_modules/justified-layout/lib/index.js
+++ npm_assets/node_modules/justified-layout/lib/index.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2016 Yahoo Inc.
+ * Copyright 2019 SmugMug, Inc.
  * Licensed under the terms of the MIT license. Please see LICENSE file in the project root for terms.
  * @license
  */
