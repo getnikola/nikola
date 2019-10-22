e5c3a1bfa7015cdd27d905d4c3563f85700a8535
Niko Wenselowski <der@nik0.de>
2019-10-22 20:46:13 +0200

    Switch test to pytest.

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import unittest
-
 from nikola.plugins.command.version import CommandVersion
 
 
-class CommandVersionCallTest(unittest.TestCase):
-    def test_version(self):
-        """Test `nikola version`."""
-        CommandVersion().execute()
+def test_version():
+    """Test `nikola version`."""
+    CommandVersion().execute()
