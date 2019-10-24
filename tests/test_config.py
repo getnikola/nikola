a95b36f31462c7b6492a2c81ccb2bd2e3fbb168e
Niko Wenselowski <der@nik0.de>
2019-10-24 20:58:54 +0200

    Move tests to the top of the module.

--- tests/test_config.py
+++ tests/test_config.py
@@ -7,9 +7,27 @@ from nikola import __main__ as nikola
 import pytest
 
 
-@pytest.fixture
-def metadata_option():
-    return "ADDITIONAL_METADATA"
+def test_simple_config(simple_config, metadata_option):
+    """Check whether configuration-files without ineritance are interpreted correctly."""
+    assert simple_config[metadata_option]["ID"] == "conf"
+
+
+def test_inherited_config(simple_config, metadata_option, complex_config):
+    """Check whether configuration-files with ineritance are interpreted correctly."""
+    check_base_equality(simple_config, metadata_option, complex_config)
+    assert complex_config[metadata_option]["ID"] == "prod"
+
+
+def test_config_with_illegal_filename(simple_config, metadata_option, complex_filename_config):
+    """Check whether files with illegal module-name characters can be set as config-files, too."""
+    check_base_equality(simple_config, metadata_option, complex_filename_config)
+    assert complex_filename_config[metadata_option]["ID"] == "illegal"
+
+
+@pytest.fixture(scope="module")
+def simple_config(test_dir):
+    nikola.main(["--conf=" + os.path.join(test_dir, "conf.py")])
+    return nikola.config
 
 
 @pytest.fixture(scope="module")
@@ -18,10 +36,9 @@ def test_dir():
     return os.path.join(script_root, "data", "test_config")
 
 
-@pytest.fixture(scope="module")
-def simple_config(test_dir):
-    nikola.main(["--conf=" + os.path.join(test_dir, "conf.py")])
-    return nikola.config
+@pytest.fixture
+def metadata_option():
+    return "ADDITIONAL_METADATA"
 
 
 @pytest.fixture(scope="module")
@@ -41,20 +58,3 @@ def check_base_equality(base_config, metadata_option, config):
     for option in base_config.keys():
         if re.match("^[A-Z]+(_[A-Z]+)*$", option) and option != metadata_option:
             assert base_config[option] == config[option]
-
-
-def test_simple_config(simple_config, metadata_option):
-    """Check whether configuration-files without ineritance are interpreted correctly."""
-    assert simple_config[metadata_option]["ID"] == "conf"
-
-
-def test_inherited_config(simple_config, metadata_option, complex_config):
-    """Check whether configuration-files with ineritance are interpreted correctly."""
-    check_base_equality(simple_config, metadata_option, complex_config)
-    assert complex_config[metadata_option]["ID"] == "prod"
-
-
-def test_config_with_illegal_filename(simple_config, metadata_option, complex_filename_config):
-    """Check whether files with illegal module-name characters can be set as config-files, too."""
-    check_base_equality(simple_config, metadata_option, complex_filename_config)
-    assert complex_filename_config[metadata_option]["ID"] == "illegal"
