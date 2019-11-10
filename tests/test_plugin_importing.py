39753f1566c2f79afbf72b3b3827564f3bfe16b6
Niko Wenselowski <der@nik0.de>
2019-11-10 18:54:22 +0100

    Tests for the Wordpress import also test this functionality.

--- tests/test_plugin_importing.py
+++ tests/test_plugin_importing.py
@@ -1,6 +1,2 @@
-def test_importing_command_import_wordpress():
-    import nikola.plugins.command.import_wordpress  # NOQA
-
-
 def test_importing_plugin_task_galleries():
     import nikola.plugins.task.galleries  # NOQA
