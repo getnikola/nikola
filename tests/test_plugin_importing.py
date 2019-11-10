adfb00d4ffc1a8a49af1db4e97aa414c9c6852b5
Niko Wenselowski <der@nik0.de>
2019-11-10 18:53:18 +0100

    We have tests that make use of the rest extension and the import is made there.

--- tests/test_plugin_importing.py
+++ tests/test_plugin_importing.py
@@ -2,9 +2,5 @@ def test_importing_command_import_wordpress():
     import nikola.plugins.command.import_wordpress  # NOQA
 
 
-def test_importing_compile_rest():
-    import nikola.plugins.compile.rest  # NOQA
-
-
 def test_importing_plugin_task_galleries():
     import nikola.plugins.task.galleries  # NOQA
