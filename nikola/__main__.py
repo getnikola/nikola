getnikola/nikola

commit d939b8864af4e55bde42eeeae93612555fc82eae

Rework Config-File Handling (#3244)

nikola/__main__.py
@@ -26,6 +26,7 @@
 
 """The main function of Nikola."""
 
+import imp
 import os
 import shutil
 import sys
@@ -55,9 +56,6 @@ try:
     import readline  # NOQA
 except ImportError:
     pass  # This is only so raw_input/input does nicer things if it's available
-
-
-import importlib.machinery
 
 config = {}
 
@@ -117,15 +115,26 @@ def main(args=None):
             os.chdir(root)
         # Help and imports don't require config, but can use one if it exists
         needs_config_file = (argname != 'help') and not argname.startswith('import_')
+        if needs_config_file:
+            if root is None:
+                LOGGER.error("The command could not be executed: You're not in a nikola website.")
+                return 1
+            else:
+                LOGGER.info("Website root: '{0}'".format(root))
     else:
         needs_config_file = False
 
-    sys.path.append('')
+    old_path = sys.path
+    old_modules = sys.modules
+
     try:
-        loader = importlib.machinery.SourceFileLoader("conf", conf_filename)
-        conf = loader.load_module()
-        config = conf.__dict__
+        sys.path = sys.path[:]
+        sys.modules = sys.modules.copy()
+        sys.path.insert(0, os.path.dirname(conf_filename))
+        with open(conf_filename, "rb") as file:
+            config = imp.load_module(conf_filename, file, conf_filename, (None, "rb", imp.PY_SOURCE)).__dict__
     except Exception:
+        config = {}
         if os.path.exists(conf_filename):
             msg = traceback.format_exc(0)
             LOGGER.error('"{0}" cannot be parsed.\n{1}'.format(conf_filename, msg))
@@ -133,7 +142,9 @@ def main(args=None):
         elif needs_config_file and conf_filename_changed:
             LOGGER.error('Cannot find configuration file "{0}".'.format(conf_filename))
             return 1
-        config = {}
+    finally:
+        sys.path = old_path
+        sys.modules = old_modules
 
     if conf_filename_changed:
         LOGGER.info("Using config file '{0}'".format(conf_filename))

dodo.py
@@ -1,7 +1,6 @@
 
 import os
 import fnmatch
-import subprocess
 
 DOIT_CONFIG = {
     'default_tasks': ['flake8', 'test'],

tests/data/test_config/conf.py
@@ -1,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import time
+
+BLOG_AUTHOR = "Your Name"
+BLOG_TITLE = "Demo Site"
+SITE_URL = "https://example.com/"
+BLOG_EMAIL = "joe@demo.site"
+BLOG_DESCRIPTION = "This is a demo site for Nikola."
+DEFAULT_LANG = "en"
+CATEGORY_ALLOW_HIERARCHIES = False
+CATEGORY_OUTPUT_FLAT_HIERARCHY = False
+HIDDEN_CATEGORIES = []
+HIDDEN_AUTHORS = ['Guest']
+LICENSE = ""
+
+CONTENT_FOOTER_FORMATS = {
+    DEFAULT_LANG: (
+        (),
+        {
+            "email": BLOG_EMAIL,
+            "author": BLOG_AUTHOR,
+            "date": time.gmtime().tm_year,
+            "license": LICENSE
+        }
+    )
+}
+
+ADDITIONAL_METADATA = {
+    "ID": "conf"
+}

tests/data/test_config/config.with+illegal(module)name.characters.py
@@ -1,0 +1,6 @@
+import conf
+
+globals().update(vars(conf))
+ADDITIONAL_METADATA = {
+    "ID": "illegal"
+}

tests/data/test_config/prod.py
@@ -1,0 +1,6 @@
+import conf
+
+globals().update(vars(conf))
+ADDITIONAL_METADATA = {
+    "ID": "prod"
+}

tests/test_config.py
@@ -1,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+
+from nikola import __main__ as nikola
+
+from .base import BaseTestCase
+
+
+class ConfigTest(BaseTestCase):
+    """Provides tests for the configuration-file handling."""
+    @classmethod
+    def setUpClass(self):
+        self.metadata_option = "ADDITIONAL_METADATA"
+        script_root = os.path.dirname(__file__)
+        test_dir = os.path.join(script_root, "data", "test_config")
+        nikola.main(["--conf=" + os.path.join(test_dir, "conf.py")])
+        self.simple_config = nikola.config
+        nikola.main(["--conf=" + os.path.join(test_dir, "prod.py")])
+        self.complex_config = nikola.config
+        nikola.main(["--conf=" + os.path.join(test_dir, "config.with+illegal(module)name.characters.py")])
+        self.complex_filename_config = nikola.config
+        self.check_base_equality(self.complex_filename_config)
+
+    @classmethod
+    def check_base_equality(self, config):
+        """Check whether the specified `config` equals the base config."""
+        for option in self.simple_config.keys():
+            if re.match("^[A-Z]+(_[A-Z]+)*$", option) and option != self.metadata_option:
+                assert self.simple_config[option] == self.complex_config[option]
+
+    def test_simple_config(self):
+        """Check whether configuration-files without ineritance are interpreted correctly."""
+        assert self.simple_config[self.metadata_option]["ID"] == "conf"
+
+    def test_inherited_config(self):
+        """Check whether configuration-files with ineritance are interpreted correctly."""
+        self.check_base_equality(config=self.complex_config)
+        assert self.complex_config[self.metadata_option]["ID"] == "prod"
+
+    def test_config_with_illegal_filename(self):
+        """Check whether files with illegal module-name characters can be set as config-files, too."""
+        self.check_base_equality(config=self.complex_filename_config)
+        assert self.complex_filename_config[self.metadata_option]["ID"] == "illegal"

commit 57dbc6a12093f9df49ea21e275369e558f649b87

Use new importlib features to import config
Issue #3314 revealed that the new config import method using `imp`,
introduced in #3244, was broken if the config file imported other
stuff. An attempt to fix this (commit 1638149f5) led to unusual,
although mostly consistent, test failures, caused by sites building
partially. Moreover, the `imp` module was deprecated in Python 3.4.
New versions of the docs suggest that the replacement is
`importlib.util.module_from_spec`, which was introduced in 3.5. The
new implementation of config file loading does everything correctly:
the broken integration tests pass, the site on which #3314 manifested
builds, and `test_config.py` (which tests the new features, and would
fail in v8.0.2) passes as well.

nikola/__main__.py
@@ -26,7 +26,7 @@
 
 """The main function of Nikola."""
 
-import imp
+import importlib.util
 import os
 import shutil
 import sys
@@ -56,6 +56,7 @@ try:
     import readline  # NOQA
 except ImportError:
     pass  # This is only so raw_input/input does nicer things if it's available
+
 
 config = {}
 
@@ -119,15 +120,16 @@ def main(args=None):
     else:
         needs_config_file = False
 
-    old_path = sys.path
-
+    sys.path.insert(0, os.path.dirname(conf_filename))
     try:
-        sys.path = sys.path[:]
-        sys.path.insert(0, os.path.dirname(conf_filename))
-        with open(conf_filename, "rb") as file:
-            config = imp.load_module(conf_filename, file, conf_filename, (None, "rb", imp.PY_SOURCE)).__dict__
+        spec = importlib.util.spec_from_file_location("conf", conf_filename)
+        conf = importlib.util.module_from_spec(spec)
+        # Preserve caching behavior of `import conf` if the filename matches
+        if os.path.splitext(os.path.basename(conf_filename))[0] == "conf":
+            sys.modules["conf"] = conf
+        spec.loader.exec_module(conf)
+        config = conf.__dict__
     except Exception:
-        config = {}
         if os.path.exists(conf_filename):
             msg = traceback.format_exc()
             LOGGER.error('"{0}" cannot be parsed.\n{1}'.format(conf_filename, msg))
@@ -135,8 +137,7 @@ def main(args=None):
         elif needs_config_file and conf_filename_changed:
             LOGGER.error('Cannot find configuration file "{0}".'.format(conf_filename))
             return 1
-    finally:
-        sys.path = old_path
+        config = {}
 
     if conf_filename_changed:
         LOGGER.info("Using config file '{0}'".format(conf_filename))