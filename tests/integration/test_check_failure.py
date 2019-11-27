169bc74fedade6b41aa486c0e320f18c99d661a0
Niko Wenselowski <der@nik0.de>
2019-11-27 18:11:23 +0100

    Migrate tests that check failing check command to pytest.

---
+++ tests/integration/test_check_failure.py
@@ -0,0 +1,76 @@
+"""The demo build should pass 'nikola check' and fail with missing files."""
+
+import io
+import os
+import shutil
+
+import pytest
+
+import nikola.plugins.command.init
+from nikola import __main__
+
+from .helper import target_dir, output_dir, fixIssue438, localeborg_setup  # NOQA
+from ..base import cd
+
+
+def test_check_links_fail(build, output_dir, target_dir):  # NOQA
+    os.unlink(os.path.join(output_dir, "archive.html"))
+
+    with cd(target_dir):
+        result = __main__.main(['check', '-l'])
+        assert result != 0
+
+
+def test_check_files_fail(build, output_dir, target_dir):  # NOQA
+    manually_added_file = os.path.join(output_dir, "foobar")
+    with io.open(manually_added_file, "w+", encoding="utf8") as outf:
+        outf.write("foo")
+
+    with cd(target_dir):
+        result = __main__.main(['check', '-f'])
+        assert result != 0
+
+
+def test_index_in_sitemap(build, output_dir):  # NOQA
+    sitemap_path = os.path.join(output_dir, "sitemap.xml")
+    with io.open(sitemap_path, "r", encoding="utf8") as inf:
+        sitemap_data = inf.read()
+
+    assert '<loc>https://example.com/</loc>' in sitemap_data
+
+
+def test_avoid_double_slash_in_rss(build, output_dir):  # NOQA
+    rss_path = os.path.join(output_dir, "rss.xml")
+    with io.open(rss_path, "r", encoding="utf8") as inf:
+        rss_data = inf.read()
+
+    assert 'https://example.com//' not in rss_data
+
+
+def test_archive_exists(build, output_dir):  # NOQA
+    """Ensure the build did something."""
+    index_path = os.path.join(output_dir, "archive.html")
+    assert os.path.isfile(index_path)
+
+
+@pytest.fixture
+def build(target_dir):  # NOQA
+    """Fill the site with demo content and build it."""
+    init_command = nikola.plugins.command.init.CommandInit()
+    init_command.copy_sample_site(target_dir)
+    init_command.create_configuration(target_dir)
+
+    src1 = os.path.join(os.path.dirname(__file__),
+                        '..', 'data', '1-nolinks.rst')
+    dst1 = os.path.join(target_dir, 'posts', '1.rst')
+    shutil.copy(src1, dst1)
+    # File for Issue #374 (empty post text)
+    with io.open(os.path.join(target_dir, 'posts', 'empty.txt'), "w+", encoding="utf8") as outf:
+        outf.write(
+            ".. title: foobar\n"
+            ".. slug: foobar\n"
+            ".. date: 2013-03-06 19:08:15\n"
+        )
+
+    with cd(target_dir):
+        __main__.main(["build"])
