3ccc00ed22d437571b463fc5a49107d070b67aa1
Niko Wenselowski <der@nik0.de>
2019-11-27 14:07:40 +0100

    Group all tests for relative links together.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -96,45 +96,6 @@ class DemoBuildTest(BaseTestCase):
         self.assertFalse('https://example.com//' in rss_data)
 
 
-class RelativeLinkTest(DemoBuildTest):
-    """Check that SITE_URL with a path doesn't break links."""
-
-    @classmethod
-    def patch_site(self):
-        """Set the SITE_URL to have a path"""
-        conf_path = os.path.join(self.target_dir, "conf.py")
-        with io.open(conf_path, "r", encoding="utf-8") as inf:
-            data = inf.read()
-            data = data.replace('SITE_URL = "https://example.com/"',
-                                'SITE_URL = "https://example.com/foo/bar/"')
-        with io.open(conf_path, "w+", encoding="utf8") as outf:
-            outf.write(data)
-
-    def test_relative_links(self):
-        """Check that the links in output/index.html are correct"""
-        test_path = os.path.join(self.target_dir, "output", "index.html")
-        flag = False
-        with io.open(test_path, "rb") as inf:
-            data = inf.read()
-            for _, _, url, _ in lxml.html.iterlinks(data):
-                # Just need to be sure this one is ok
-                if url.endswith("css"):
-                    self.assertFalse(url.startswith(".."))
-                    flag = True
-        # But I also need to be sure it is there!
-        self.assertTrue(flag)
-
-    def test_index_in_sitemap(self):
-        """Test that the correct path is in sitemap, and not the wrong one."""
-        sitemap_path = os.path.join(self.target_dir, "output", "sitemap.xml")
-        with io.open(sitemap_path, "r", encoding="utf8") as inf:
-            sitemap_data = inf.read()
-        self.assertFalse('<loc>https://example.com/</loc>' in sitemap_data)
-        self.assertTrue(
-            '<loc>https://example.com/foo/bar/</loc>' in
-            sitemap_data)
-
-
 class TestCheck(DemoBuildTest):
     """The demo build should pass 'nikola check'"""
 
@@ -215,6 +176,45 @@ class TestCheckFailure(DemoBuildTest):
 
             result = __main__.main(['check', '-f'])
             assert result != 0
+
+
+class RelativeLinkTest(DemoBuildTest):
+    """Check that SITE_URL with a path doesn't break links."""
+
+    @classmethod
+    def patch_site(self):
+        """Set the SITE_URL to have a path"""
+        conf_path = os.path.join(self.target_dir, "conf.py")
+        with io.open(conf_path, "r", encoding="utf-8") as inf:
+            data = inf.read()
+            data = data.replace('SITE_URL = "https://example.com/"',
+                                'SITE_URL = "https://example.com/foo/bar/"')
+        with io.open(conf_path, "w+", encoding="utf8") as outf:
+            outf.write(data)
+
+    def test_relative_links(self):
+        """Check that the links in output/index.html are correct"""
+        test_path = os.path.join(self.target_dir, "output", "index.html")
+        flag = False
+        with io.open(test_path, "rb") as inf:
+            data = inf.read()
+            for _, _, url, _ in lxml.html.iterlinks(data):
+                # Just need to be sure this one is ok
+                if url.endswith("css"):
+                    self.assertFalse(url.startswith(".."))
+                    flag = True
+        # But I also need to be sure it is there!
+        self.assertTrue(flag)
+
+    def test_index_in_sitemap(self):
+        """Test that the correct path is in sitemap, and not the wrong one."""
+        sitemap_path = os.path.join(self.target_dir, "output", "sitemap.xml")
+        with io.open(sitemap_path, "r", encoding="utf8") as inf:
+            sitemap_data = inf.read()
+        self.assertFalse('<loc>https://example.com/</loc>' in sitemap_data)
+        self.assertTrue(
+            '<loc>https://example.com/foo/bar/</loc>' in
+            sitemap_data)
 
 
 class RelativeLinkTest2(DemoBuildTest):
