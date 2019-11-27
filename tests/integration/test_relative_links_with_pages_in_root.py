9a1fac2ffc49619c128e36103ae5da928e29493b
Niko Wenselowski <der@nik0.de>
2019-11-27 17:45:38 +0100

    Tests for relative links have been migrated to pytest.

---
+++ tests/integration/test_relative_links_with_pages_in_root.py
@@ -0,0 +1,95 @@
+"""Check that dropping pages to the root doesn't break links."""
+
+import io
+import os
+import shutil
+
+import lxml
+import pytest
+
+import nikola.plugins.command.init
+from nikola import __main__
+
+from .helper import target_dir, output_dir, fixIssue438, localeborg_setup  # NOQA
+from ..base import cd
+
+
+def test_relative_links(build, output_dir):  # NOQA
+    """Check that the links in a page are correct"""
+    test_path = os.path.join(output_dir, "about-nikola.html")
+
+    flag = False
+    with io.open(test_path, "rb") as inf:
+        data = inf.read()
+        for _, _, url, _ in lxml.html.iterlinks(data):
+            # Just need to be sure this one is ok
+            if url.endswith("css"):
+                assert not url.startswith("..")
+                flag = True
+
+    # But I also need to be sure it is there!
+    assert flag
+
+
+def test_index_in_sitemap(build, output_dir):  # NOQA
+    """Test that the correct path is in sitemap, and not the wrong one."""
+    sitemap_path = os.path.join(output_dir, "sitemap.xml")
+    with io.open(sitemap_path, "r", encoding="utf8") as inf:
+        sitemap_data = inf.read()
+
+    assert '<loc>https://example.com/</loc>' not in sitemap_data
+    assert '<loc>https://example.com/blog/index.html</loc>' in sitemap_data
+
+
+def test_avoid_double_slash_in_rss(build, output_dir):  # NOQA
+    rss_path = os.path.join(output_dir, "rss.xml")
+    with io.open(rss_path, "r", encoding="utf8") as inf:
+        rss_data = inf.read()
+
+    assert 'https://example.com//' not in rss_data
+
+
+def test_archive_exists(build, output_dir):  # NOQA
+    """Ensure the build did something."""
+    index_path = os.path.join(output_dir, "archive.html")
+    assert os.path.isfile(index_path)
+
+
+@pytest.fixture
+def build(target_dir):  # NOQA
+    """Fill the site with demo content and build it."""
+    init_command = nikola.plugins.command.init.CommandInit()
+    init_command.copy_sample_site(target_dir)
+    init_command.create_configuration(target_dir)
+
+    src1 = os.path.join(os.path.dirname(__file__),
+                        '..', 'data', '1-nolinks.rst')
+    dst1 = os.path.join(target_dir, 'posts', '1.rst')
+    shutil.copy(src1, dst1)
+    # File for Issue #374 (empty post text)
+    with io.open(os.path.join(target_dir, 'posts', 'empty.txt'), "w+", encoding="utf8") as outf:
+        outf.write(
+            ".. title: foobar\n"
+            ".. slug: foobar\n"
+            ".. date: 2013-03-06 19:08:15\n"
+        )
+
+    # Configure our pages to reside in the root
+    conf_path = os.path.join(target_dir, "conf.py")
+    with io.open(conf_path, "r", encoding="utf-8") as inf:
+        data = inf.read()
+
+    data = data.replace('("pages/*.txt", "pages", "page.tmpl"),',
+                        '("pages/*.txt", "", "page.tmpl"),')
+    data = data.replace('("pages/*.rst", "pages", "page.tmpl"),',
+                        '("pages/*.rst", "", "page.tmpl"),')
+    data = data.replace('# INDEX_PATH = ""',
+                        'INDEX_PATH = "blog"')
+    data += "\nPRETTY_URLS = False\nSTRIP_INDEXES = False"
+
+    with io.open(conf_path, "w+", encoding="utf8") as outf:
+        outf.write(data)
+        outf.flush()
+
+    with cd(target_dir):
+        __main__.main(["build"])
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,8 +5,6 @@ import sys
 import tempfile
 import unittest
 
-import lxml.html
-
 import nikola
 import nikola.plugins.command
 import nikola.plugins.command.init
@@ -113,91 +111,6 @@ class TestCheckFailure(DemoBuildTest):
 
             result = __main__.main(['check', '-f'])
             assert result != 0
-
-
-class RelativeLinkTest(DemoBuildTest):
-    """Check that SITE_URL with a path doesn't break links."""
-
-    @classmethod
-    def patch_site(self):
-        """Set the SITE_URL to have a path"""
-        conf_path = os.path.join(self.target_dir, "conf.py")
-        with io.open(conf_path, "r", encoding="utf-8") as inf:
-            data = inf.read()
-            data = data.replace('SITE_URL = "https://example.com/"',
-                                'SITE_URL = "https://example.com/foo/bar/"')
-        with io.open(conf_path, "w+", encoding="utf8") as outf:
-            outf.write(data)
-
-    def test_relative_links(self):
-        """Check that the links in output/index.html are correct"""
-        test_path = os.path.join(self.target_dir, "output", "index.html")
-        flag = False
-        with io.open(test_path, "rb") as inf:
-            data = inf.read()
-            for _, _, url, _ in lxml.html.iterlinks(data):
-                # Just need to be sure this one is ok
-                if url.endswith("css"):
-                    self.assertFalse(url.startswith(".."))
-                    flag = True
-        # But I also need to be sure it is there!
-        self.assertTrue(flag)
-
-    def test_index_in_sitemap(self):
-        """Test that the correct path is in sitemap, and not the wrong one."""
-        sitemap_path = os.path.join(self.target_dir, "output", "sitemap.xml")
-        with io.open(sitemap_path, "r", encoding="utf8") as inf:
-            sitemap_data = inf.read()
-        self.assertFalse('<loc>https://example.com/</loc>' in sitemap_data)
-        self.assertTrue(
-            '<loc>https://example.com/foo/bar/</loc>' in
-            sitemap_data)
-
-
-class RelativeLinkTest2(DemoBuildTest):
-    """Check that dropping pages to the root doesn't break links."""
-
-    @classmethod
-    def patch_site(self):
-        """Set the SITE_URL to have a path"""
-        conf_path = os.path.join(self.target_dir, "conf.py")
-        with io.open(conf_path, "r", encoding="utf-8") as inf:
-            data = inf.read()
-            data = data.replace('("pages/*.txt", "pages", "page.tmpl"),',
-                                '("pages/*.txt", "", "page.tmpl"),')
-            data = data.replace('("pages/*.rst", "pages", "page.tmpl"),',
-                                '("pages/*.rst", "", "page.tmpl"),')
-            data = data.replace('# INDEX_PATH = ""',
-                                'INDEX_PATH = "blog"')
-            data += "\nPRETTY_URLS = False\nSTRIP_INDEXES = False"
-        with io.open(conf_path, "w+", encoding="utf8") as outf:
-            outf.write(data)
-            outf.flush()
-
-    def test_relative_links(self):
-        """Check that the links in a page are correct"""
-        test_path = os.path.join(
-            self.target_dir, "output", "about-nikola.html")
-        flag = False
-        with io.open(test_path, "rb") as inf:
-            data = inf.read()
-            for _, _, url, _ in lxml.html.iterlinks(data):
-                # Just need to be sure this one is ok
-                if url.endswith("css"):
-                    self.assertFalse(url.startswith(".."))
-                    flag = True
-        # But I also need to be sure it is there!
-        self.assertTrue(flag)
-
-    def test_index_in_sitemap(self):
-        """Test that the correct path is in sitemap, and not the wrong one."""
-        sitemap_path = os.path.join(self.target_dir, "output", "sitemap.xml")
-        with io.open(sitemap_path, "r", encoding="utf8") as inf:
-            sitemap_data = inf.read()
-        self.assertFalse('<loc>https://example.com/</loc>' in sitemap_data)
-        self.assertTrue(
-            '<loc>https://example.com/blog/index.html</loc>'
-            in sitemap_data)
 
 
 if __name__ == "__main__":
