8601d28eae3f2c47f4a95b753fec73beec93c0d8
Niko Wenselowski <der@nik0.de>
2019-12-16 12:44:06 +0100

    Basic functions should all exist in the normal test module.

--- tests/integration/test_page_index_pretty_urls.py
+++ tests/integration/test_page_index_pretty_urls.py
@@ -1,20 +1,18 @@
 """Test if PAGE_INDEX works, with different PRETTY_URLS=True."""
 
-import io
 import os
 
 import pytest
 
 import nikola.plugins.command.init
 from nikola import __main__
-from nikola.utils import makedirs
 
 from ..base import cd
 from .helper import append_config
 from .test_empty_build import (  # NOQA
     test_archive_exists, test_avoid_double_slash_in_rss, test_check_files,
     test_check_links, test_index_in_sitemap)
-from .test_page_index_normal_urls import create_pages
+from .test_page_index_normal_urls import check_build_output, create_pages
 
 
 def test_page_index(build, output_dir):
@@ -25,63 +23,6 @@ def test_page_index(build, output_dir):
         return os.path.join(dir, name + '/index.html')
 
     check_build_output(output_dir, output_path)
-
-
-def check_build_output(output_dir, path_func):
-    pages = os.path.join(output_dir, "pages")
-    subdir1 = os.path.join(output_dir, "pages", "subdir1")
-    subdir2 = os.path.join(output_dir, "pages", "subdir2")
-    subdir3 = os.path.join(output_dir, "pages", "subdir3")
-
-    # Do all files exist?
-    assert os.path.isfile(path_func(pages, 'page0'))
-    assert os.path.isfile(path_func(subdir1, 'page1'))
-    assert os.path.isfile(path_func(subdir1, 'page2'))
-    assert os.path.isfile(path_func(subdir2, 'page3'))
-    assert os.path.isfile(path_func(subdir3, 'page4'))
-
-    assert os.path.isfile(os.path.join(pages, 'index.html'))
-    assert os.path.isfile(os.path.join(subdir1, 'index.html'))
-    assert os.path.isfile(os.path.join(subdir2, 'index.html'))
-    assert os.path.isfile(os.path.join(subdir3, 'index.php'))
-    assert not os.path.isfile(os.path.join(subdir3, 'index.html'))
-
-    # Do the indexes only contain the pages the should?
-    with io.open(os.path.join(pages, 'index.html'), 'r', encoding='utf-8') as fh:
-        pages_index = fh.read()
-    assert 'Page 0' in pages_index
-    assert 'Page 1' not in pages_index
-    assert 'Page 2' not in pages_index
-    assert 'Page 3' not in pages_index
-    assert 'Page 4' not in pages_index
-    assert 'This is not the page index' not in pages_index
-
-    with io.open(os.path.join(subdir1, 'index.html'), 'r', encoding='utf-8') as fh:
-        subdir1_index = fh.read()
-    assert 'Page 0' not in subdir1_index
-    assert 'Page 1' in subdir1_index
-    assert 'Page 2' in subdir1_index
-    assert 'Page 3' not in subdir1_index
-    assert 'Page 4' not in subdir1_index
-    assert 'This is not the page index' not in subdir1_index
-
-    with io.open(os.path.join(subdir2, 'index.html'), 'r', encoding='utf-8') as fh:
-        subdir2_index = fh.read()
-    assert 'Page 0' not in subdir2_index
-    assert 'Page 1' not in subdir2_index
-    assert 'Page 2' not in subdir2_index
-    assert 'Page 3' not in subdir2_index
-    assert 'Page 4' not in subdir2_index
-    assert 'This is not the page index.' in subdir2_index
-
-    with io.open(os.path.join(subdir3, 'index.php'), 'r', encoding='utf-8') as fh:
-        subdir3_index = fh.read()
-    assert 'Page 0' not in subdir3_index
-    assert 'Page 1' not in subdir3_index
-    assert 'Page 2' not in subdir3_index
-    assert 'Page 3' not in subdir3_index
-    assert 'Page 4' not in subdir3_index
-    assert 'This is not the page index either.' in subdir3_index
 
 
 @pytest.fixture(scope="module")
