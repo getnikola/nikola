ad1a9c0c279dddaa6d6779c7668f92756e856216
Niko Wenselowski <der@nik0.de>
2019-12-27 00:54:23 +0100

    Less repeated code for setting up the demo site contents through all the tests.

--- tests/integration/test_archive_full.py
+++ tests/integration/test_archive_full.py
@@ -1,14 +1,13 @@
 """Check that full archives build and are correct."""
 
 import os
-import shutil
 
 import pytest
 
-import nikola.plugins.command.init
 from nikola import __main__
 
-from .helper import add_post_without_text, cd, patch_config
+from .helper import cd, patch_config
+from .test_demo_build import prepare_demo_site
 from .test_empty_build import (  # NOQA
     test_avoid_double_slash_in_rss,
     test_check_files,
@@ -33,17 +32,9 @@ def test_full_archive(build, output_dir, path):
 
 
 @pytest.fixture(scope="module")
-def build(target_dir, test_dir):
+def build(target_dir):
     """Fill the site with demo content and build it."""
-    init_command = nikola.plugins.command.init.CommandInit()
-    init_command.copy_sample_site(target_dir)
-    init_command.create_configuration(target_dir)
-
-    src1 = os.path.join(test_dir, "..", "data", "1-nolinks.rst")
-    dst1 = os.path.join(target_dir, "posts", "1.rst")
-    shutil.copy(src1, dst1)
-
-    add_post_without_text(os.path.join(target_dir, "posts"))
+    prepare_demo_site(target_dir)
 
     patch_config(
         target_dir, ("# CREATE_FULL_ARCHIVES = False", "CREATE_FULL_ARCHIVES = True")
--- tests/integration/test_check_failure.py
+++ tests/integration/test_check_failure.py
@@ -2,14 +2,13 @@
 
 import io
 import os
-import shutil
 
 import pytest
 
-import nikola.plugins.command.init
 from nikola import __main__
 
-from .helper import add_post_without_text, cd
+from .helper import cd
+from .test_demo_build import prepare_demo_site
 from .test_empty_build import (  # NOQA
     test_avoid_double_slash_in_rss,
     test_index_in_sitemap,
@@ -35,17 +34,9 @@ def test_check_files_fail(build, output_dir, target_dir):
 
 
 @pytest.fixture(scope="module")
-def build(target_dir, test_dir):
+def build(target_dir):
     """Fill the site with demo content and build it."""
-    init_command = nikola.plugins.command.init.CommandInit()
-    init_command.copy_sample_site(target_dir)
-    init_command.create_configuration(target_dir)
-
-    src1 = os.path.join(test_dir, "..", "data", "1-nolinks.rst")
-    dst1 = os.path.join(target_dir, "posts", "1.rst")
-    shutil.copy(src1, dst1)
-
-    add_post_without_text(os.path.join(target_dir, "posts"))
+    prepare_demo_site(target_dir)
 
     with cd(target_dir):
         __main__.main(["build"])
