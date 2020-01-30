e1d4b8894b71b380f4a40e686e5f3e49d96a4ef0
Marcel Stimberg <marcel.stimberg@inserm.fr>
2020-01-30 11:49:35 +0100

    Pass correct parameters to shortcodes in ipynb
    The wrong call passed a boolean as the filename and the language
    as the post.

--- nikola/plugins/compile/ipynb.py
+++ nikola/plugins/compile/ipynb.py
@@ -85,7 +85,9 @@ class CompileIPynb(PageCompiler):
         with io.open(dest, "w+", encoding="utf8") as out_file:
             with io.open(source, "r", encoding="utf8") as in_file:
                 nb_str = in_file.read()
-            output, shortcode_deps = self.compile_string(nb_str, is_two_file, post, lang)
+            output, shortcode_deps = self.compile_string(nb_str, source,
+                                                         is_two_file, post,
+                                                         lang)
             out_file.write(output)
         if post is None:
             if shortcode_deps:
