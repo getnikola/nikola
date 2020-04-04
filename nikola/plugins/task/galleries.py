8e4f0c56dd74d6462f55c34ce09d866860577b50
Roberto Alsina <ralsina@netmanagers.com.ar>
2020-04-04 17:51:10 -0300

    Fix #3360 (#3369)
    * Fix #3360

--- nikola/plugins/task/galleries.py
+++ nikola/plugins/task/galleries.py
@@ -503,8 +503,9 @@ class Galleries(Task, ImageProcessor):
             # index.txt file would be errorneously named `index`
             # (warning: galleries titled index and filenamed differently
             #  may break)
-            if post.title == 'index':
-                post.title = os.path.split(gallery)[1]
+            if post.title() == 'index':
+                for lang in post.meta.keys():
+                    post.meta[lang]['title'] = os.path.split(gallery)[1]
             # Register the post (via #2417)
             self.site.post_per_input_file[index_path] = post
         else:
