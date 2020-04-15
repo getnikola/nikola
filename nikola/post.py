bd9fba21db3bd72bbd5fb0f1f9af97c830f5c400
Roberto Alsina <ralsina@netmanagers.com.ar>
2020-04-15 19:16:13 -0300

    Add a source('lang') method to Post (#3390)
    Fix #3384

--- nikola/post.py
+++ nikola/post.py
@@ -748,9 +748,23 @@ class Post(object):
             real_lang = sorted(self.translated_to)[0]
             return get_translation_candidate(self.config, self.base_path, real_lang), real_lang
 
+    def source(self, lang=None):
+        """Read the post and return its source."""
+        if lang is None:
+            lang = nikola.utils.LocaleBorg().current_lang
+
+        source = self.translated_source_path(lang)
+        with open(source, encoding='utf-8') as inf:
+            data = inf.read()
+        if self.is_two_file:  # Metadata is not here
+            source_data = data
+        else:
+            source_data = self.compiler.split_metadata(data, self, lang)[1]
+        return source_data
+
     def text(self, lang=None, teaser_only=False, strip_html=False, show_read_more_link=True,
              feed_read_more_link=False, feed_links_append_query=None):
-        """Read the post file for that language and return its contents.
+        """Read the post file for that language and return its compiled contents.
 
         teaser_only=True breaks at the teaser marker and returns only the teaser.
         strip_html=True removes HTML tags
