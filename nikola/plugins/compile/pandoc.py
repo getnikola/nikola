bf042483a0019afb1a3a440de5f06aaa323c5a96
Kolen Cheung <christian.kolen@gmail.com>
2021-01-13 15:30:08 -0800

    pandoc.py: for single-quote hater

--- nikola/plugins/compile/pandoc.py
+++ nikola/plugins/compile/pandoc.py
@@ -52,7 +52,7 @@ class CompilePandoc(PageCompiler):
         super().set_site(site)
 
     def _get_pandoc_options(self, source: str) -> List[str]:
-        '''obtain pandoc args from config depending on type and file extensions'''
+        """obtain pandoc args from config depending on type and file extensions"""
         # Union[List[str], Dict[str, List[str]]]
         config_options = self.site.config['PANDOC_OPTIONS']
         type_ = type(config_options)
