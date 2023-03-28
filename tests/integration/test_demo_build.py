91a68b7e4f21770d3fd6e0582c01f0ecaea5ea7d
Jonathan Hartley <tartley@tartley.com>
2023-03-27 23:22:22 -0500

    Add a test of the Gallery RSS content
    I have a few outstanding questions, marked 'TODO', to resolve in
    code review.

--- tests/integration/test_demo_build.py
+++ tests/integration/test_demo_build.py
@@ -10,6 +10,7 @@ settings.
 import os
 
 import pytest
+import rss_parser
 
 import nikola.plugins.command.init
 from nikola import __main__
@@ -22,6 +23,66 @@ from .test_empty_build import (  # NOQA
     test_check_links,
     test_index_in_sitemap,
 )
+
+
+def to_dict(obj, *attrs):
+    """Return a dict composed of the given attrs, taken from obj"""
+    return {name: getattr(obj, name) for name in attrs}
+
+
+def test_gallery_rss(build, output_dir):
+    # Given a build of the demo samplesite in 'output_dir'
+    # When we look for the RSS file of the "Demo" gallery
+    # TODO Can I rely on the name of 'galleries' during tests,
+    #      or should I be using/setting config->GALLERY_FOLDERS?
+    rss_path = os.path.join(output_dir, 'galleries', 'demo', 'rss.xml')
+    # Then it exists
+    assert os.path.isfile(rss_path)
+    # and it contains text
+    with open(rss_path) as fp:
+        content = fp.read()
+    assert isinstance(content, str)
+    assert len(content) > 0
+    # and the text is valid RSS
+    parsed = rss_parser.Parser(xml=content).parse()
+    # and the RSS contains gallery attributes:
+    # TODO Should the title be the name of the gallery, not the final image in it?
+    assert parsed.title == 'Tesla conducts lg'
+    assert parsed.version == '2.0'
+    assert parsed.language == 'en'
+    # TODO Should the gallery description contain the content of the index.txt file?
+    assert parsed.description == ''
+    # and the image items in the RSS feed are:
+    expected_items = [
+        dict(
+            title='Tesla4 lg',
+            link='https://example.com/galleries/demo/tesla4_lg.jpg',
+            publish_date='Wed, 01 Jan 2014 00:01:00 GMT',
+        ),
+        dict(
+            title='Tesla lightning1 lg',
+            link='https://example.com/galleries/demo/tesla_lightning1_lg.jpg',
+            publish_date='Wed, 01 Jan 2014 00:03:00 GMT',
+        ),
+        dict(
+            title='Tesla lightning2 lg',
+            link='https://example.com/galleries/demo/tesla_lightning2_lg.jpg',
+            publish_date='Wed, 01 Jan 2014 00:04:00 GMT',
+        ),
+        dict(
+            title='Tesla tower1 lg',
+            link='https://example.com/galleries/demo/tesla_tower1_lg.jpg',
+            publish_date='Wed, 01 Jan 2014 00:05:00 GMT',
+        ),
+        dict(
+            title='Tesla conducts lg',
+            link='https://example.com/galleries/demo/tesla_conducts_lg.webp',
+            publish_date='Mon, 27 Mar 2023 18:18:55 GMT',
+        ),
+    ]
+    for item, expected in zip(parsed.feed, expected_items):
+        actual = to_dict(item, 'title', 'link', 'publish_date')
+        assert actual == expected
 
 
 @pytest.fixture(scope="module")
