8775ccd5cfc5c09d1a460c209dcd128d9cd582d2
Jonathan Hartley <tartley@tartley.com>
2023-03-30 09:03:26 -0500

    Switch the new gallery RSS test to use feedparser
    The previous library, rss_parser, doesn't successfully parse enclosures:
    https://github.com/dhvcc/rss-parser/issues/37
    
    I switched to feedparser, changed the test to use their format of
    returned parsed data structure. I still need to tell it to grab the
    (custom?) field of the gallery description, but everything else seems to
    work.

--- tests/integration/test_demo_build.py
+++ tests/integration/test_demo_build.py
@@ -10,8 +10,9 @@ settings.
 import itertools
 import os
 
+import feedparser
+import freezegun
 import pytest
-import rss_parser
 
 import nikola.plugins.command.init
 from nikola import __main__
@@ -24,11 +25,6 @@ from .test_empty_build import (  # NOQA
     test_check_links,
     test_index_in_sitemap,
 )
-
-
-def to_dict(obj, *attrs):
-    """Return a dict composed of the given attrs, taken from obj"""
-    return {name: getattr(obj, name, '<missing>') for name in attrs}
 
 
 def test_gallery_rss(build, output_dir):
@@ -44,49 +40,60 @@ def test_gallery_rss(build, output_dir):
         content = fp.read()
     assert isinstance(content, str)
     assert len(content) > 0
-    # and the text is valid RSS
-    parsed = rss_parser.Parser(xml=content).parse()
-    # and the RSS contains gallery attributes:
+    # and the text can be parsed as valid RSS
+    parsed = feedparser.parse(content)
+    from pprint import pprint
+    pprint(parsed)
+    # and the RSS contains top level attributes:
+    assert parsed.version == 'rss20'
+    # and the RSS contains feed attributes about the gallery:
+    assert parsed.feed.language == 'en'
+    assert parsed.feed.link == 'https://example.com/galleries/demo/rss.xml'
     # TODO Should the title be the name of the gallery, not the final image in it?
-    assert parsed.title == 'Tesla tower1 lg'
-    assert parsed.version == '2.0'
-    assert parsed.language == 'en'
+    assert parsed.feed.title == 'Tesla tower1 lg'
+    assert parsed.feed.updated == 'Wed, 05 Apr 2023 23:59:58 GMT'
     # TODO Should the gallery description contain the content of the index.txt file?
-    assert parsed.description == ''
+    # assert parsed.description == ''
+
     # and the image items in the RSS feed are:
     expected_items = [
         dict(
-            title='Tesla4 lg',
+            id='galleries/demo/tesla4_lg.jpg',
             link='https://example.com/galleries/demo/tesla4_lg.jpg',
-            publish_date='Wed, 01 Jan 2014 00:01:00 GMT',
+            published='Wed, 01 Jan 2014 00:01:00 GMT',
+            title='Tesla4 lg',
         ),
         dict(
-            title='Tesla conducts lg',
+            id='galleries/demo/tesla_conducts_lg.webp',
             link='https://example.com/galleries/demo/tesla_conducts_lg.webp',
-            publish_date='Wed, 01 Jan 2014 00:02:00 GMT',
+            published='Wed, 01 Jan 2014 00:02:00 GMT',
+            title='Tesla conducts lg',
         ),
         dict(
-            title='Tesla lightning1 lg',
+            id='galleries/demo/tesla_lightning1_lg.jpg',
             link='https://example.com/galleries/demo/tesla_lightning1_lg.jpg',
-            publish_date='Wed, 01 Jan 2014 00:03:00 GMT',
+            published='Wed, 01 Jan 2014 00:03:00 GMT',
+            title='Tesla lightning1 lg',
         ),
         dict(
-            title='Tesla lightning2 lg',
+            id='galleries/demo/tesla_lightning2_lg.jpg',
             link='https://example.com/galleries/demo/tesla_lightning2_lg.jpg',
-            publish_date='Wed, 01 Jan 2014 00:04:00 GMT',
+            published='Wed, 01 Jan 2014 00:04:00 GMT',
+            title='Tesla lightning2 lg',
         ),
         dict(
-            title='Tesla tower1 lg',
+            id='galleries/demo/tesla_tower1_lg.jpg',
             link='https://example.com/galleries/demo/tesla_tower1_lg.jpg',
-            publish_date='Wed, 01 Jan 2014 00:05:00 GMT',
+            published='Wed, 01 Jan 2014 00:05:00 GMT',
+            title='Tesla tower1 lg',
         ),
     ]
-    for index, (actual_obj, expected) in enumerate(
-        itertools.zip_longest(parsed.feed, expected_items)
+    for index, (actual, expected) in enumerate(
+        itertools.zip_longest(parsed.entries, expected_items)
     ):
-        actual = to_dict(actual_obj, 'title', 'link', 'publish_date')
-        assert actual == expected, f'item {index} mismatch'
-
+        # Assert keys/values in expected are a subset of those in actual.
+        assert expected.items() <= actual.items(), f'item {index} mismatch'
+    # TODO can we get the gallery description from the XML?
 
 @pytest.fixture(scope="module")
 def build(target_dir):
@@ -94,7 +101,8 @@ def build(target_dir):
     prepare_demo_site(target_dir)
 
     with cd(target_dir):
-        __main__.main(["build"])
+        with freezegun.freeze_time("2023-04-05 23:59:58", tz_offset=0):
+            __main__.main(["build"])
 
 
 def prepare_demo_site(target_dir):
