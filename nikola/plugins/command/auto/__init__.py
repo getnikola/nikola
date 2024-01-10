a14af60603656eee427d7c9a4663074ec19fdefa
Chris Warrick <kwpolska@gmail.com>
2024-01-10 20:46:37 +0100

    2024! :fireworks:

--- nikola/plugins/command/auto/__init__.py
+++ nikola/plugins/command/auto/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright © 2012-2023 Chris Warrick, Roberto Alsina and others.
+# Copyright © 2012-2024 Chris Warrick, Roberto Alsina and others.
 
 # Permission is hereby granted, free of charge, to any
 # person obtaining a copy of this software and associated
--- nikola/plugins/compile/ipynb.py
+++ nikola/plugins/compile/ipynb.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright © 2013-2023 Damián Avila, Chris Warrick and others.
+# Copyright © 2013-2024 Damián Avila, Chris Warrick and others.
 
 # Permission is hereby granted, free of charge, to any
 # person obtaining a copy of this software and associated
