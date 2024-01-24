60f5f1aa9f35239d3023566286c83d4086d238fa
fondbcn <wakilobarca11@gmail.com>
2024-01-24 10:47:21 +0100

    replacing pkg_resources with importlib

--- nikola/nikola.py
+++ nikola/nikola.py
@@ -46,7 +46,7 @@ import lxml.etree
 import lxml.html
 import natsort
 import PyRSS2Gen as rss
-import importlib_resources
+from importlib import resources
 from blinker import signal
 
 from . import DEBUG, SHOW_TRACEBACKS, filters, utils, hierarchy_utils, shortcodes
@@ -1031,7 +1031,7 @@ class Nikola(object):
         extra_plugins_dirs = self.config['EXTRA_PLUGINS_DIRS']
         self._loading_commands_only = commands_only
         self._plugin_places = [
-            importlib_resources('nikola') / 'plugins',
+            resources.files('nikola').joinpath('plugins'),
             os.path.expanduser(os.path.join('~', '.nikola', 'plugins')),
             os.path.join(os.getcwd(), 'plugins'),
         ] + [path for path in extra_plugins_dirs if path]
@@ -1694,7 +1694,7 @@ class Nikola(object):
         """
         self.register_shortcode('template', self._template_shortcode_handler)
 
-        builtin_sc_dir = importlib_resources('nikola') / os.path.join('data', 'shortcodes', utils.get_template_engine(self.THEMES))
+        builtin_sc_dir = resources.files('nikola').joinpath(os.path.join('data', 'shortcodes', utils.get_template_engine(self.THEMES)))
 
         for sc_dir in [builtin_sc_dir, 'shortcodes']:
             if not os.path.isdir(sc_dir):
