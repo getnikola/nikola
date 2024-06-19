e534c04532138891812250686dbfb17bfb931e2f
Abdul Aziz Hatkou <zizochampion@hotmail.com>
2024-06-19 17:06:09 +0200

    Add check test test_plugins.py

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -11,6 +11,16 @@ def test_command_version():
 
     CommandVersion().execute()
 
+def test_command_version_check():
+    """Test `nikola version --check`."""
+    from nikola.plugins.command.version import CommandVersion
+
+    CommandVersion().execute({'check': True})
+
+    CommandVersion().execute({'check': True, 'old': True})
+
+    CommandVersion.print_command_version_coverage()
+
 
 def test_importing_plugin_task_galleries():
     import nikola.plugins.task.galleries  # NOQA
