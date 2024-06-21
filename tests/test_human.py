31b36611cd645759cccdb0834f8ef939ffaae692
Darian <daj-samsoedien@live.nl>
2024-06-21 10:50:46 +0200

    finished the human time test

--- tests/test_human.py
+++ tests/test_human.py
@@ -1,34 +1,41 @@
 import pytest
 from datetime import timedelta
-from nikola.plugins.command.status import CommandStatus 
+from nikola.plugins.command.status import CommandStatus
 
 @pytest.fixture(scope="module")
 def command_status():
     return CommandStatus()
 
-
+@pytest.mark.individual
 def test_days_branch(command_status):
-    command_status.reset_coverage()
+    print("test wit 2 days 3 hours:")
     result = command_status.human_time(timedelta(days=2, hours=3))
-    print(result)
-    command_status.report_coverage()
+    print(f"this is the result: ${result}")
 
+@pytest.mark.individual
 def test_hours_branch(command_status):
-    command_status.reset_coverage()
+    print("test with 4 hours 20 minutes:")
     result = command_status.human_time(timedelta(hours=4, minutes=20))
-    print(result)
-    command_status.report_coverage()
+    print(f"this is the result: ${result}")
 
+
+@pytest.mark.individual
 def test_minutes_branch(command_status):
-    command_status.reset_coverage()
+    print("test with 15 minutes:")
     result = command_status.human_time(timedelta(minutes=15))
-    print(result)
-    command_status.report_coverage()
+    print(f"this is the result: ${result}")
 
+@pytest.mark.individual
 def test_seconds_branch(command_status):
-    command_status.reset_coverage()
+    print("test with 0 seconds:")
     result = command_status.human_time(timedelta(seconds=0))
-    print(result)
-    command_status.report_coverage()
+    print(f"this is the result: ${result}")
 
+def test_combined(command_status):
+    command_status.reset_coverage()
+    test_days_branch(command_status)
+    test_hours_branch(command_status)
+    test_minutes_branch(command_status)
+    test_seconds_branch(command_status)
+    command_status.report_coverage()
 
