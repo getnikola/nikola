ffc2dc079ee9bff300b522be8630bbf14abe75ca
Andreas Krüger <andreas.krueger@famsik.de>
2024-12-19 23:47:07 +0100

    pydocstyle improvement.

--- nikola/plugins/command/basepath_helper.py
+++ nikola/plugins/command/basepath_helper.py
@@ -1,5 +1,6 @@
-import urllib
+"""Extracting the base path from site['SITE_URL'] or site['BASE_URL']."""
 
+import urllib
 
 def base_path_from_siteuri(siteuri: str) -> str:
     """Extract the path part from a URI such as site['SITE_URL'].
