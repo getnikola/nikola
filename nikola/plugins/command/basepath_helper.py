430a556166ff1d614e0243f8e5fc39987c4e5c13
Andreas Krüger <andreas.krueger@famsik.de>
2024-12-19 23:59:28 +0100

    Comment cosmetics.

--- nikola/plugins/command/basepath_helper.py
+++ nikola/plugins/command/basepath_helper.py
@@ -2,10 +2,11 @@
 
 import urllib
 
+
 def base_path_from_siteuri(siteuri: str) -> str:
     """Extract the path part from a URI such as site['SITE_URL'].
 
-    The path never ends with a "/". (If only "/" is intended, it is empty.)
+    The path returned doesn't end with a "/". (If only "/" is intended, it is empty.)
     """
     path = urllib.parse.urlsplit(siteuri).path
     if path.endswith("/"):
